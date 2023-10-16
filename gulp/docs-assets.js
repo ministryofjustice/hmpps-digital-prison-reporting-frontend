43dfc157335fc28e1029fedfb344053a42a7dc0f
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-10-16 10:39:53 +0100

    [DPR2-68] Fix documentation JS

--- gulp/docs-assets.js
+++ gulp/docs-assets.js
@@ -2,6 +2,6 @@ const gulp = require("gulp");
 
 gulp.task('docs:assets', async () => {
   return gulp
-    .src("docs/**/*.+(png|jpg|jpeg|gif|svg|ico|woff2)")
-    .pipe(gulp.dest("package/dpr/"));
+    .src("docs/assets/**/*.*")
+    .pipe(gulp.dest("package/dpr/assets/"));
 });

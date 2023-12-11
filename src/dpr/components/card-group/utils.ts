631d7b9967f8d6ed0a98f67d02404702224c7985
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2023-12-11 17:32:37 +0000

    [DPR2-198] Card mapping utils

---
+++ src/dpr/components/card-group/utils.ts
@@ -0,0 +1,47 @@
+import { components } from '../../types/api'
+import querystring from 'querystring'
+
+export default {
+  reportDefinitionsToCards: (reportDefinitions: Array<components['schemas']['ReportDefinition']>, pathPrefix: String) => {
+    return reportDefinitions.map((d: components['schemas']['ReportDefinition']) => ({
+      text: d.name,
+      href: `${pathPrefix}/${d.id}`,
+      description: d.description,
+    }))
+  },
+
+  variantDefinitionsToCards: (reportDefinition: components['schemas']['ReportDefinition'], pathPrefix: String, filterPrefix: String) => {
+    return reportDefinition.variants.map((v: components['schemas']['VariantDefinition']) => {
+      const defaultFilters: Record<string, string> = {}
+
+      v.specification.fields
+        .filter(f => f.filter && f.filter.defaultValue)
+        .forEach(f => {
+          if (f.filter.type === 'daterange') {
+            const dates = f.filter.defaultValue.split(' - ')
+
+            if (dates.length >= 1) {
+              // eslint-disable-next-line prefer-destructuring
+              defaultFilters[`${filterPrefix}${f.name}.start`] = dates[0]
+
+              if (dates.length >= 2) {
+                // eslint-disable-next-line prefer-destructuring
+                defaultFilters[`${filterPrefix}${f.name}.end`] = dates[1]
+              }
+            }
+          } else {
+            defaultFilters[`${filterPrefix}${f.name}`] = f.filter.defaultValue
+          }
+        })
+
+      const path = `${pathPrefix}/${reportDefinition.id}/${v.id}`
+      const query = querystring.stringify(defaultFilters)
+
+      return {
+        text: v.name,
+        href: query.length === 0 ? path : `${path}?${query}`,
+        description: v.description,
+      }
+    })
+  }
+}

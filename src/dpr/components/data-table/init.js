e9ff9a2b60e7ad6ded97f7a108dbe4ae401cf840
rossdowthwaite <rossdowthwaite@gmail.com>
2024-01-26 16:46:41 +0000

    fix int tests

--- src/dpr/components/data-table/init.js
+++ src/dpr/components/data-table/init.js
@@ -8,27 +8,29 @@ $('[data-navigate-to]').each((index, element) => {
 
 function createTableHeaderAndFooter() {
   const table = document.getElementById('dpr-data-table')
-  const { classification, length } = table.dataset
+  if (table) {
+    const { classification, length } = table.dataset
 
-  const header = table.createTHead()
-  const footer = table.createTFoot()
+    const header = table.createTHead()
+    const footer = table.createTFoot()
 
-  const headerRow = header.insertRow(0)
-  const footerRow = footer.insertRow(0)
+    const headerRow = header.insertRow(0)
+    const footerRow = footer.insertRow(0)
 
-  const headerCell = headerRow.insertCell(0)
-  const footerCell = footerRow.insertCell(0)
+    const headerCell = headerRow.insertCell(0)
+    const footerCell = footerRow.insertCell(0)
 
-  const classList = 'govuk-table__header print-header-footer'
-  const content = `<b>${classification}</b>`
+    const classList = 'govuk-table__header print-header-footer'
+    const content = `<b>${classification}</b>`
 
-  headerCell.colSpan = length
-  headerCell.classList = classList
-  headerCell.innerHTML = content
+    headerCell.colSpan = length
+    headerCell.classList = classList
+    headerCell.innerHTML = content
 
-  footerCell.colSpan = length
-  footerCell.classList = classList
-  footerCell.innerHTML = content
+    footerCell.colSpan = length
+    footerCell.classList = classList
+    footerCell.innerHTML = content
+  }
 }
 
 createTableHeaderAndFooter()

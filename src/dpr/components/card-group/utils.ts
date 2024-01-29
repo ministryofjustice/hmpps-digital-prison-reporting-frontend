70d3ff3bab7bf0b75550ecdeeb3a7a659a559ddc
Stu Pegg <stuart.pegg@digital.justice.gov.uk>
2024-01-29 17:43:14 +0000

    [DPR2-282] Update API types

--- src/dpr/components/card-group/utils.ts
+++ src/dpr/components/card-group/utils.ts
@@ -2,18 +2,18 @@ import { components } from '../../types/api'
 
 export default {
   reportDefinitionsToCards: (
-    reportDefinitions: Array<components['schemas']['ReportDefinition']>,
+    reportDefinitions: Array<components['schemas']['ReportDefinitionSummary']>,
     pathPrefix: string,
   ) => {
-    return reportDefinitions.map((d: components['schemas']['ReportDefinition']) => ({
+    return reportDefinitions.map((d: components['schemas']['ReportDefinitionSummary']) => ({
       text: d.name,
       href: `${pathPrefix}/${d.id}`,
       description: d.description,
     }))
   },
 
-  variantDefinitionsToCards: (reportDefinition: components['schemas']['ReportDefinition'], pathPrefix: string) => {
-    return reportDefinition.variants.map((v: components['schemas']['VariantDefinition']) => ({
+  variantDefinitionsToCards: (reportDefinition: components['schemas']['ReportDefinitionSummary'], pathPrefix: string) => {
+    return reportDefinition.variants.map((v: components['schemas']['VariantDefinitionSummary']) => ({
       text: v.name,
       href: `${pathPrefix}/${reportDefinition.id}/${v.id}`,
       description: v.description,

d1b56065a1944642e667ff58601f794669527c82
rossdowthwaite <rossdowthwaite@gmail.com>
2024-02-01 16:46:28 +0000

    filter accordion

--- src/dpr/setUpNunjucksFilters.ts
+++ src/dpr/setUpNunjucksFilters.ts
@@ -1,6 +1,6 @@
 import nunjucks from 'nunjucks'
 import createUrlForParameters from './utils/urlHelper'
-import getTodayIsoDate from './components/filters2/today'
+import getTodayIsoDate from './components/filters/today'
 
 const setUpNunjucksFilters = (env: nunjucks.Environment) => {
   env.addFilter('createUrlForParameters', createUrlForParameters)

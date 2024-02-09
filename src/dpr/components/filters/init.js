c1ba6b76640537143f91ea7ffc8482d835a27662
rossdowthwaite <rossdowthwaite@gmail.com>
2024-02-09 11:48:58 +0000

    fix tests

--- src/dpr/components/filters/init.js
+++ src/dpr/components/filters/init.js
@@ -1,34 +1,3 @@
-/* global $ */
-
-// $(function () {
-// $('[data-apply-form-to-querystring=true]').on('click', function () {
-//   const formSelector = $(this).data('apply-form-selector')
-//   const filtersRegExp = /filters[.\w]+=[^&]*/g
-//   const pagingRegExp = /paging\.selectedPage=\d+/
-//   const ampRexExp = /([&])\1+/g
-
-//   let url = decodeURI(window.location.href)
-//     .replaceAll(filtersRegExp, '')
-//     .replace(pagingRegExp, 'paging.selectedPage=1')
-
-//   url += url.indexOf('?') === -1 ? '?' : '&'
-//   url += $(formSelector).serialize()
-//   url = url.replaceAll('?&', '?').replaceAll(ampRexExp, '&')
-
-//   window.location.href = url
-// })
-
-//   $('[data-reset-filters=true]').on('click', function () {
-//     const resetColsRegExp = /&?columns=[^&]*/g
-//     const columnsQuery = window.location.href.match(resetColsRegExp)
-//     const baseUrl = $(this).data('apply-base-url')
-
-//     let url = baseUrl
-//     if (columnsQuery) url += columnsQuery.join('')
-//     window.location.href = url
-//   })
-// })
-
 const applyFiltersButton = document.body.querySelector('[data-apply-form-to-querystring=true]')
 if (applyFiltersButton) {
   applyFiltersButton.addEventListener('click', (e) => {

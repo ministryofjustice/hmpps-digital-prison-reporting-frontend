6a1d35835188df871560884548960c6601eec4c4
rossdowthwaite <rossdowthwaite@gmail.com>
2024-03-14 10:31:18 +0000

    updated loading state to new preferred version

--- src/dpr/DprLoadingClientClass.mjs
+++ src/dpr/DprLoadingClientClass.mjs
@@ -5,52 +5,11 @@ export default class DprLoadingClientClass extends DprClientClass {
     document.querySelectorAll('.loading-panel').forEach((l) => {
       l.classList.add('show')
     })
-
-    document.querySelectorAll('button').forEach((b) => {
-      b.disabled = true
-    })
-
-    document.querySelectorAll('.accordion-summary-remove-button').forEach((b) => {
-      b.setAttribute('disabled', true)
-    })
-
-    document.querySelectorAll('.govuk-pagination__link').forEach((b) => {
-      b.setAttribute('disabled', true)
-      b.classList.add('disabled')
-    })
-
-    const pageSizeElement = document.getElementById('pageSize')
-    if (pageSizeElement) {
-      pageSizeElement.setAttribute('disabled', true)
-    }
-
-    document.querySelectorAll('.govuk-details.accordion-details').forEach((detailsButton) => {
-      detailsButton.classList.add('disabled')
-    })
   }
 
   hideLoadingAnimation() {
     document.querySelectorAll('.loading-panel').forEach((l) => {
       l.classList.remove('show')
     })
-
-    document.querySelectorAll('button').forEach((b) => {
-      b.disabled = false
-    })
-
-    document.querySelectorAll('.accordion-summary-remove-button').forEach((b) => {
-      b.setAttribute('disabled', false)
-    })
-
-    document.querySelectorAll('.govuk-pagination__link').forEach((b) => {
-      console.log(b)
-      b.setAttribute('disabled', false)
-      b.classList.remove('disabled')
-    })
-
-    const pageSizeElement = document.getElementById('pageSize')
-    if (pageSizeElement) {
-      pageSizeElement.setAttribute('disabled', false)
-    }
   }
 }

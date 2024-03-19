8f64ff8cb66dbaff299b9bf5eccc377da18800c6
rossdowthwaite <rossdowthwaite@gmail.com>
2024-03-19 13:33:59 +0000

    card loading state

--- src/dpr/DprLoadingClientClass.mjs
+++ src/dpr/DprLoadingClientClass.mjs
@@ -1,14 +1,24 @@
 import { DprClientClass } from './DprClientClass.mjs'
 
 export default class DprLoadingClientClass extends DprClientClass {
-  showLoadingAnimation() {
-    document.querySelectorAll('.loading-panel').forEach((l) => {
+  showLoadingAnimation(wrapperClass) {
+    const loadingPanelClass = '.loading-panel'
+    document.querySelectorAll(loadingPanelClass).forEach((l) => {
+      l.classList.add('show')
+    })
+
+    let loadingAnimClass = '.dpr-loading-anim'
+    if (wrapperClass) loadingAnimClass = `.${wrapperClass} ${loadingAnimClass}`
+    document.querySelectorAll(loadingAnimClass).forEach((l) => {
       l.classList.add('show')
     })
   }
 
   hideLoadingAnimation() {
     document.querySelectorAll('.loading-panel').forEach((l) => {
+      l.classList.remove('show')
+    })
+    document.querySelectorAll('.dpr-loading-anim').forEach((l) => {
       l.classList.remove('show')
     })
   }
--- src/dpr/components/card-group/clientClass.mjs
+++ src/dpr/components/card-group/clientClass.mjs
@@ -1,15 +1,19 @@
-import { DprClientClass } from '../../DprClientClass.mjs'
+import DprLoadingClientClass from '../../DprLoadingClientClass.mjs'
 
-export default class CardGroup extends DprClientClass {
+export default class CardGroup extends DprLoadingClientClass {
   static getModuleName() {
     return 'card-group'
   }
 
   initialise() {
+    this.hideLoadingAnimation()
     this.getElement()
       .querySelectorAll('[data-click-navigate-to]')
       .forEach((card) => {
         card.addEventListener('click', () => {
+          const wrapperClass = 'card-loading'
+          card.classList.add(wrapperClass)
+          this.showLoadingAnimation(wrapperClass)
           window.location.href = card.dataset.clickNavigateTo
         })
       })

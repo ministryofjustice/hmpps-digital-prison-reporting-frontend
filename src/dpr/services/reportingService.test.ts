8a35205629ca3699dc57246fdaf039fd7fb50925
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-06-11 15:56:36 +0100

    Dpr2 775 asyc reports components (#103)
    Added Reporting Service to FE lib so could accurately use the Type

---
+++ src/dpr/services/reportingService.test.ts
@@ -0,0 +1,55 @@
+import ReportingClient from '../data/reportingClient'
+import { components } from '../types/api'
+import ReportingService from './reportingService'
+import Dict = NodeJS.Dict
+
+jest.mock('../data/reportingClient')
+
+describe('Reporting service', () => {
+  let reportingClient: jest.Mocked<ReportingClient>
+  let reportingService: ReportingService
+
+  beforeEach(() => {
+    reportingClient = new ReportingClient(null) as jest.Mocked<ReportingClient>
+    reportingService = new ReportingService(reportingClient)
+  })
+
+  describe('getCount', () => {
+    it('Retrieves count from client', async () => {
+      const expectedCount = 456
+      reportingClient.getCount.mockResolvedValue(expectedCount)
+
+      const result = await reportingService.getCount(null, null, null)
+
+      expect(result).toEqual(expectedCount)
+    })
+  })
+
+  describe('getList', () => {
+    it('Retrieves list from client', async () => {
+      const expectedResponse: Array<Dict<string>> = [{ test: 'true' }]
+      reportingClient.getList.mockResolvedValue(expectedResponse)
+
+      const result = await reportingService.getList(null, null, null)
+
+      expect(result).toEqual(expectedResponse)
+    })
+  })
+
+  describe('getDefinitions', () => {
+    it('Retrieves definitions from client', async () => {
+      const expectedResponse: Array<components['schemas']['ReportDefinitionSummary']> = [
+        {
+          id: 'test-report',
+          name: 'Test report',
+          variants: [],
+        },
+      ]
+      reportingClient.getDefinitions.mockResolvedValue(expectedResponse)
+
+      const result = await reportingService.getDefinitions(null)
+
+      expect(result).toEqual(expectedResponse)
+    })
+  })
+})
---
+++ src/dpr/services/reportingService.ts
@@ -0,0 +1,55 @@
+import { components } from '../types/api'
+import type ReportingClient from '../data/reportingClient'
+import ReportQuery from '../types/ReportQuery'
+import Dict = NodeJS.Dict
+
+export default class ReportingService {
+  constructor(private readonly reportingClient: ReportingClient) {}
+
+  async getCount(resourceName: string, token: string, listRequest: ReportQuery): Promise<number> {
+    return this.reportingClient.getCount(resourceName, token, listRequest)
+  }
+
+  async getList(resourceName: string, token: string, listRequest: ReportQuery): Promise<Array<NodeJS.Dict<string>>> {
+    return this.reportingClient.getList(resourceName, token, listRequest)
+  }
+
+  async getDefinitions(
+    token: string,
+    dataProductDefinitionsPath?: string,
+  ): Promise<Array<components['schemas']['ReportDefinitionSummary']>> {
+    return this.reportingClient.getDefinitions(token, dataProductDefinitionsPath)
+  }
+
+  async requestAsyncReport(
+    token: string,
+    reportId: string,
+    variantId: string,
+    query: Record<string, string | boolean | number>,
+  ): Promise<Dict<string>> {
+    return this.reportingClient.requestAsyncReport(token, reportId, variantId, query)
+  }
+
+  async getAsyncReport(
+    token: string,
+    reportId: string,
+    variantId: string,
+    tableId: string,
+    query: Dict<string | number>,
+  ): Promise<Array<Dict<string>>> {
+    return this.reportingClient.getAsyncReport(token, reportId, variantId, tableId, query)
+  }
+
+  async getAsyncReportStatus(
+    token: string,
+    reportId: string,
+    variantId: string,
+    executionId: string,
+  ): Promise<Dict<string>> {
+    return this.reportingClient.getAsyncReportStatus(token, reportId, variantId, executionId)
+  }
+
+  async getAsyncCount(token: string, tableId: string): Promise<number> {
+    return this.reportingClient.getAsyncCount(token, tableId)
+  }
+}

f65bca59e63f6a5c0cd66f49447d90c19f3866f2
Stu Pegg <110236995+stalwartsolutions@users.noreply.github.com>
2024-06-20 21:33:59 +0100

    [DPR2-833] Integration tests for Search (#122)

---
+++ cypress-tests/integration-tests/pages/SearchPage.ts
@@ -0,0 +1,7 @@
+export type PageElement = Cypress.Chainable<JQuery>
+
+export default class SearchPage {
+  textBox = (): PageElement => cy.get(`.dpr-search-box`)
+
+  results = (): PageElement => cy.get(`.dpr-search-table tbody tr:not(.search-option-hide)`)
+}

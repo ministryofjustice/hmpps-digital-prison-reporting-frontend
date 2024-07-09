c7edc3dfba33509151c2511bf94d82982b85b750
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-07-09 15:35:12 +0100

    Dpr2 919 consolidate reports (#156)
    Consolidate Requested reports:
    https://dsdmoj.atlassian.net/jira/software/c/projects/DPR2/boards/1345?selectedIssue=DPR2-919
    
    - check for duplicate requests helper function + tests
    - Implemented on report request
    - Refactor filters utils
    - Improved error handling messaging
    - Filter bug fix

--- src/dpr/utils/reportSummaryHelper.ts
+++ src/dpr/utils/reportSummaryHelper.ts
@@ -1,4 +1,5 @@
 import { CardData } from '../components/table-card-group/types'
+import { AsyncReportData } from '../types/AsyncReport'
 
 export const createDetailsHtml = (title: string, content: string) => {
   return `<details class="govuk-details">
@@ -16,4 +17,26 @@ export const createDetailsHtml = (title: string, content: string) => {
 export const createSummaryHtml = (card: CardData) => {
   const summaryHtml = card.summary.map((item) => `<li class="govuk-body-s">${item.name}: ${item.value}</li>`).join('')
   return `<ul class="dpr-card-group__item__filters-list">${summaryHtml}</ul>`
+}
+
+export const getDuplicateRequestIds = (newReportSearchParams: string, existingReports: AsyncReportData[]) => {
+  const duplicates: string[] = []
+  const newQueryParams = new URLSearchParams(newReportSearchParams)
+
+  existingReports.forEach((existingReportData: AsyncReportData) => {
+    const matches: boolean[] = []
+    const existingQueryParams = new URLSearchParams(existingReportData.url.request.search)
+
+    if (existingQueryParams.entries.length === newQueryParams.entries.length) {
+      newQueryParams.forEach((newValue, newKey) => {
+        const match = existingQueryParams.has(newKey, newValue)
+        matches.push(match)
+      })
+    }
+    if (matches.every(Boolean)) {
+      duplicates.push(existingReportData.executionId)
+    }
+  })
+
+  return duplicates
 }

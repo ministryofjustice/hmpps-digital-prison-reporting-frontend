0c1cc53cb2d6627a393b4303f84c7d29d2b5d99e
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-08-21 16:33:20 +0100

    fixed sort (#189)
    Fix to sort Report list alphabetically

--- src/dpr/components/reports-list/utils.ts
+++ src/dpr/components/reports-list/utils.ts
@@ -2,6 +2,14 @@ import { Response } from 'express'
 import { components } from '../../types/api'
 import { Services } from '../../types/Services'
 
+interface variantData {
+  reportName: string
+  reportId: string
+  variantId: string
+  variantName: string
+  variantDescription: string
+  reportDescription: string
+}
 export default {
   mapReportsList: (
     res: Response,
@@ -9,34 +17,47 @@ export default {
   ): { head: { text: string }[]; rows: { text?: string; html?: string }[] } => {
     const { definitions, csrfToken } = res.locals
     const pathSuffix = res.locals.pathSuffix || ''
-    const rows = definitions.flatMap((d: components['schemas']['ReportDefinitionSummary']) => {
-      const { id: reportId, name: reportName, description: reportDescription } = d
-      return d.variants
-        .sort(
-          (
-            a: components['schemas']['ReportDefinitionSummary'],
-            b: components['schemas']['ReportDefinitionSummary'],
-          ) => {
-            if (a.name < b.name) return -1
-            if (a.name > b.name) return 1
-            return 0
+
+    const allVariants = definitions.flatMap((def: components['schemas']['ReportDefinitionSummary']) => {
+      const { id: reportId, name: reportName, description: reportDescription } = def
+      const { variants } = def
+      return variants.map((variant) => {
+        const { id: variantId, name: variantName, description: variantDescription } = variant
+        return {
+          reportName,
+          reportId,
+          variantId,
+          variantName,
+          variantDescription,
+          reportDescription,
+        }
+      })
+    })
+
+    const sortedVariants = allVariants.sort((a: variantData, b: variantData) => {
+      if (a.variantName < b.variantName) {
+        return -1
+      }
+      if (a.variantName > b.variantName) {
+        return 1
+      }
+      return 0
+    })
+
+    const rows = sortedVariants.map((v: variantData) => {
+      const { variantId, variantName, variantDescription, reportName, reportId, reportDescription } = v
+      return [
+        { text: reportName },
+        { html: `<a href="/async-reports/${reportId}/${variantId}/request${pathSuffix}">${variantName}</a>` },
+        { text: variantDescription || reportDescription },
+        {
+          html: services.bookmarkService.createBookMarkToggleHtml(reportId, variantId, csrfToken, 'reports-list'),
+          classes: 'dpr-vertical-align',
+          attributes: {
+            tabindex: 0,
           },
-        )
-        .map((v) => {
-          const { id: variantId, name: variantName, description: variantDescription } = v
-          return [
-            { text: reportName },
-            { html: `<a href="/async-reports/${reportId}/${variantId}/request${pathSuffix}">${variantName}</a>` },
-            { text: variantDescription || reportDescription },
-            {
-              html: services.bookmarkService.createBookMarkToggleHtml(reportId, variantId, csrfToken, 'reports-list'),
-              classes: 'dpr-vertical-align',
-              attributes: {
-                tabindex: 0,
-              },
-            },
-          ]
-        })
+        },
+      ]
     })
 
     const head = [

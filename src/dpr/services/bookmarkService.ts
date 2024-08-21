5c4425ed22958e96f3d93bba77ad32c891783bd0
rossdowthwaite <rossdowthwaite@gmail.com>
2024-08-21 16:59:06 +0100

    updated ID for label

--- src/dpr/services/bookmarkService.ts
+++ src/dpr/services/bookmarkService.ts
@@ -60,8 +60,8 @@ export default class BookmarkService extends UserStoreService {
     const checked = this.isBookmarked(variantId) ? 'checked' : null
     const tooltip = !checked ? 'Add Bookmark' : 'Remove Bookmark'
     return `<div class='dpr-bookmark dpr-bookmark-tooltip dpr-bookmark-table' tooltip="${tooltip}">
-  <input class="bookmark-input" aria-label="bookmark toggle" type='checkbox' id='${variantId}-${id}' data-report-id='${reportId}' data-variant-id='${variantId}' data-csrf-token='${csrfToken}' ${checked} />
-  <label id="${variantId}-${reportId}-bookmark-label" for='${variantId}-${id}'><span class="dpr-visually-hidden">Bookmark toggle</span></label>
+  <input class="bookmark-input" aria-label="bookmark toggle" type='checkbox' id='${reportId}-${variantId}-${id}' data-report-id='${reportId}' data-variant-id='${variantId}' data-csrf-token='${csrfToken}' ${checked} />
+  <label id="${variantId}-${reportId}-bookmark-label" for='${reportId}-${variantId}-${id}'><span class="dpr-visually-hidden">Bookmark toggle</span></label>
 </div>`
   }
 }

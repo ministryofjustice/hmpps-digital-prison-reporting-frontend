b185c0d38688eb0a99929fa619bf0dc75d689bd0
rossdowthwaite <rossdowthwaite@gmail.com>
2024-09-06 16:17:33 +0100

    temp fix

--- src/dpr/components/chart-card/utils.ts
+++ src/dpr/components/chart-card/utils.ts
@@ -1,6 +1,7 @@
 import { ChartCardData } from '../../types/Charts'
 
 export default {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   getChartData: (metricDataFromAPI: any): ChartCardData => {
     // TODO: convert data from the matric Api to ChartCardData
     const convertedChartData: ChartCardData = metricDataFromAPI

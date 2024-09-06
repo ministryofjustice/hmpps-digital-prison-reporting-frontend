7309caa425feeefa8eef695ef2acde617e89eeb2
rossdowthwaite <rossdowthwaite@gmail.com>
2024-09-06 17:18:03 +0100

    mock dashbaord services

--- src/dpr/components/chart-card/utils.ts
+++ src/dpr/components/chart-card/utils.ts
@@ -1,10 +1,21 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ChartCardData } from '../../types/Charts'
 
 export default {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  getChartData: (metricDataFromAPI: any): ChartCardData => {
-    // TODO: convert data from the matric Api to ChartCardData
-    const convertedChartData: ChartCardData = metricDataFromAPI
+  /**
+   * Mock implementation of how data might be fetched and transformed
+   *
+   * @param {string} metricId
+   * @param {ChartCardData[]} mockChartsApiData -temporary, will come from an API
+   * @return {*}  {ChartCardData}
+   */
+  getChartData: async (metricId: string, mockMetricsService: any): Promise<ChartCardData> => {
+    // TODO: get data from metrics API -
+    const dataFromMetricsApi = await mockMetricsService.getMetricData(metricId)
+
+    // TODO: convert data from the metric Api to ChartCardData
+    const convertedChartData: ChartCardData = dataFromMetricsApi
+
     return convertedChartData
   },
 }
--- src/dpr/routes/dashboard.ts
+++ src/dpr/routes/dashboard.ts
@@ -16,18 +16,22 @@ export default function routes(
 ) {
   const getDashboardDataHandler: RequestHandler = async (req, res, next) => {
     try {
+      const { id } = req.params
       // TODO: get Dashboard Data from API
-      const mockDashboardData = ['multi_chart-0', 'multi_chart-2', 'line-chart-2']
+      const mockDashboardData = await DashboardService.getDashboardData(id)
 
-      const mockChartsData: ChartCardData[] = mockDashboardData.map((metricId) => {
-        // TODO: get metric data from an API
-        const MetricDataFromTheAPI = mockChartsApiData.find((chart) => chart.id === metricId)
+      const mockMetricsService = {
+        getMetricData: async (metricId: string) => {
+          return Promise.resolve(mockChartsApiData.find((chart) => chart.id === metricId))
+        },
+      }
 
-        // TODO Munge Data to conform to charts interface. munge, munge, munge
-        const mockChartData = ChartCardUtils.getChartData(MetricDataFromTheAPI)
-
-        return mockChartData
-      })
+      const mockChartsData: ChartCardData[] = await Promise.all(
+        mockDashboardData.map(async (metricId) => {
+          // TODO: implementaton to get & transform the metrics data
+          return ChartCardUtils.getChartData(metricId, mockMetricsService)
+        }),
+      )
 
       res.render(`${templatePath}dashboard`, {
         title: 'Mock Dashboard',
@@ -40,4 +44,11 @@ export default function routes(
   }
 
   router.get('/dashboard/:id', getDashboardDataHandler)
+}
+
+// Mock Dashboard service
+const DashboardService = {
+  getDashboardData: async (id: string) => {
+    return Promise.resolve(['multi_chart-0', 'multi_chart-2', 'line-chart-2'])
+  },
 }

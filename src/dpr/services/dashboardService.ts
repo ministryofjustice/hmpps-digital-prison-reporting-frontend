f28edd3053d99d2354518059af58486fd289ef33
rossdowthwaite <rossdowthwaite@gmail.com>
2024-09-09 11:23:39 +0100

    dashboard client

--- src/dpr/services/dashboardService.ts
+++ src/dpr/services/dashboardService.ts
@@ -1,7 +1,5 @@
-import MockDashboardClient from '../../../test-app/mockChartData/mockDashboardClient'
-
 export default class DashboardService {
-  constructor(private readonly mockDashboardClient: MockDashboardClient) {
+  constructor() {
     // TODO: init DahsboardClient? needs creating. Or use existing ReportingClient
   }
 
@@ -16,10 +14,34 @@ export default class DashboardService {
    * @memberof DashboardService
    */
   async getDefinition(token: string, id: string) {
-    return this.mockDashboardClient.getDefinition(token, id)
+    return mockDashboardClient.getDefinition(token, id)
   }
 
   async getDefinitions() {
-    return this.mockDashboardClient.getDefinitions()
+    return mockDashboardClient.getDefinitions()
   }
 }
+
+// MOCK DASHBOARD CLIENT
+// TODO: implement for real when API is ready
+const mockDashboardClient = {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getDefinition: async (tokenC: string, idC: string): Promise<{ id: string; metrics: { id: string }[] }> => {
+    return Promise.resolve(dashboards.find((d) => d.id === idC))
+  },
+
+  getDefinitions: async () => {
+    Promise.resolve(dashboards)
+  },
+}
+
+const dashboards = [
+  {
+    id: 'dashboard1',
+    metrics: [{ id: 'multi_chart-0' }, { id: 'multi_chart-2' }, { id: 'line-chart-2' }],
+  },
+  {
+    id: 'dashboard2',
+    metrics: [{ id: 'multi_chart-1' }, { id: 'multi_chart-0' }, { id: 'line-chart-0' }],
+  },
+]

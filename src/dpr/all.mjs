90fceb51e16f16184d44fbdf471c365c6e62cf73
rossdowthwaite <rossdowthwaite@gmail.com>
2024-10-07 16:41:37 +0100

    form validation

--- src/dpr/all.mjs
+++ src/dpr/all.mjs
@@ -21,6 +21,7 @@ import ShowMore from './components/show-more/clientClass.mjs'
 import BarChartVisualisation from './components/chart/bar/clientClass.mjs'
 import DoughnutChartVisualisation from './components/chart/doughnut/clientClass.mjs'
 import LineChartVisualisation from './components/chart/line/clientClass.mjs'
+import DownloadFeedbackForm from './components/download-feeback-form/cientClass.mjs'
 import LoadDashboard from './DprLoadDashboard.mjs'
 /**
  * Initialise all components
@@ -54,6 +55,7 @@ export default function initAll() {
     DoughnutChartVisualisation,
     LineChartVisualisation,
     LoadDashboard,
+    DownloadFeedbackForm,
   ]
 
   components.forEach((Component) => {
--- src/dpr/routes/download.ts
+++ src/dpr/routes/download.ts
@@ -1,6 +1,20 @@
 import type { RequestHandler, Router } from 'express'
 import { Services } from '../types/Services'
 
+const mockUser = {
+  username: 'RDOWTHWAITE_GEN',
+  active: true,
+  name: 'Ross Dowthwaite',
+  authSource: 'nomis',
+  staffId: 487629,
+  activeCaseLoadId: 'KMI',
+  userId: '487629',
+  uuid: '5651801a-7990-4b97-887c-95180e37edf3',
+  displayName: 'Ross Dowthwaite',
+  token:
+    'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImhtcHBzLWF1dGgtZGV2LTIwMjMwMzA2In0.eyJzdWIiOiJSRE9XVEhXQUlURV9HRU4iLCJ1c2VyX25hbWUiOiJSRE9XVEhXQUlURV9HRU4iLCJhdXRoX3NvdXJjZSI6Im5vbWlzIiwiaXNzIjoiaHR0cHM6Ly9zaWduLWluLWRldi5obXBwcy5zZXJ2aWNlLmp1c3RpY2UuZ292LnVrL2F1dGgvaXNzdWVyIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9QUklTT04iLCJST0xFX0RFTEVURV9TRU5TSVRJVkVfQ0FTRV9OT1RFUyIsIlJPTEVfUFJJU09OU19SRVBPUlRJTkdfVE9PTFNfVVNFUiIsIlJPTEVfR0xPQkFMX1NFQVJDSCIsIlJPTEVfSU5BQ1RJVkVfQk9PS0lOR1MiLCJST0xFX1BSSVNPTlNfUkVQT1JUSU5HX1VTRVIiLCJST0xFX0FERF9TRU5TSVRJVkVfQ0FTRV9OT1RFUyJdLCJjbGllbnRfaWQiOiJzdHUtcGVnZyIsInVzZXJfdXVpZCI6IjU2NTE4MDFhLTc5OTAtNGI5Ny04ODdjLTk1MTgwZTM3ZWRmMyIsImdyYW50X3R5cGUiOiJhdXRob3JpemF0aW9uX2NvZGUiLCJ1c2VyX2lkIjoiNDg3NjI5Iiwic2NvcGUiOlsicmVhZCIsIndyaXRlIl0sIm5hbWUiOiJSb3NzIERvd3Rod2FpdGUiLCJleHAiOjE3MjgzMTE1ODIsImp0aSI6Imt1eVF2RzRveUY1c3JvRVBya0VnVVlZR0w4NCJ9.LTCvWnWxA5M4PoE8HRHGMg7roud1ML-I6hIkPssnnvFeInO59PGu3A1OVMwZUMXAwFalQJ2YbKQjuKOWyakFQdp4K5LzgZCxyv2jimTBCtn7UAB8ZloJseBP4X_A_E_bSIMJUIkNYs-sjFCx0DLfEKyf7dpgMmNLuKcISgbHUT3tSGRLIxYrQJSri6Fj8JoeW5t1MOZ7lQ8r47JJwY9wL0WRVsPJ-QDBbM11MKj2Y-xmhPTWg1YRm7nOHF0BxPuENxebPOVhb4FlPzLfRSg6TbvZCLDnqost4JBOAhm7Wu0RaD38XKJ5GbHMbuWvfo_f7jnG_wQQmIq0hUnRBj7rTQ',
+}
+
 export default function routes({
   router,
   layoutPath,
@@ -13,9 +27,16 @@ export default function routes({
   templatePath?: string
 }) {
   const feedbackFormHandler: RequestHandler = async (req, res, next) => {
+    const { reportId, variantId } = req.params
     try {
+      res.locals.user = mockUser
       res.render(`${templatePath}feedback-form`, {
         title: 'Feedback Form',
+        user: res.locals.user,
+        report: {
+          reportId,
+          variantId,
+        },
         layoutPath,
         postEndpoint: '/submitFeedback/',
       })
@@ -24,8 +45,13 @@ export default function routes({
     }
   }
 
+  const feedbackSubmitHandler: RequestHandler = async (req, res, next) => {
+    const { body } = req
+    console.log({ body })
+  }
+
   router.get('/download/:dpdId/:reportid/feedback', feedbackFormHandler)
 
   // TODO: implement the post handlers for the feedback form
-  // router.post('/submitFeedback/', feedbackSubmitHandler, feedbackSubmitSuccessHandler)
+  router.post('/submitFeedback/', feedbackSubmitHandler)
 }

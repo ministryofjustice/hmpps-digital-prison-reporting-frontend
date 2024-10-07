be10d26bb86d8a62c0f4028bb846099610676d05
rossdowthwaite <rossdowthwaite@gmail.com>
2024-10-07 09:15:48 +0100

    uncomment code

--- test-app/mockAsyncData/mockVariants/variant21.js
+++ test-app/mockAsyncData/mockVariants/variant21.js
@@ -8,42 +8,42 @@ const variant21 = {
   specification: {
     template: 'list',
     fields: [
-      // {
-      //   name: 'field1',
-      //   display: 'Field 1',
-      //   sortable: true,
-      //   defaultsort: true,
-      //   type: 'string',
-      //   mandatory: false,
-      //   visible: true,
-      //   filter: {
-      //     type: 'Radio',
-      //     staticOptions: [
-      //       { name: 'value1.1', display: 'Value 1.1' },
-      //       { name: 'value1.2', display: 'Value 1.2' },
-      //       { name: 'value1.3', display: 'Value 1.3' },
-      //     ],
-      //     // defaultValue: 'value1.2',
-      //     mandatory: true,
-      //   },
-      // },
-      // {
-      //   name: 'field2',
-      //   display: 'Field 2',
-      //   sortable: true,
-      //   type: 'string',
-      //   mandatory: true,
-      //   visible: true,
-      //   filter: {
-      //     type: 'Select',
-      //     staticOptions: [
-      //       { name: 'value2.1', display: 'Value 2.1' },
-      //       { name: 'value2.2', display: 'Value 2.2' },
-      //       { name: 'value2.3', display: 'Value 2.3' },
-      //     ],
-      //     mandatory: true,
-      //   },
-      // },
+      {
+        name: 'field1',
+        display: 'Field 1',
+        sortable: true,
+        defaultsort: true,
+        type: 'string',
+        mandatory: false,
+        visible: true,
+        filter: {
+          type: 'Radio',
+          staticOptions: [
+            { name: 'value1.1', display: 'Value 1.1' },
+            { name: 'value1.2', display: 'Value 1.2' },
+            { name: 'value1.3', display: 'Value 1.3' },
+          ],
+          // defaultValue: 'value1.2',
+          mandatory: true,
+        },
+      },
+      {
+        name: 'field2',
+        display: 'Field 2',
+        sortable: true,
+        type: 'string',
+        mandatory: true,
+        visible: true,
+        filter: {
+          type: 'Select',
+          staticOptions: [
+            { name: 'value2.1', display: 'Value 2.1' },
+            { name: 'value2.2', display: 'Value 2.2' },
+            { name: 'value2.3', display: 'Value 2.3' },
+          ],
+          mandatory: true,
+        },
+      },
       {
         name: 'field3',
         display: 'Field 3',
@@ -59,44 +59,44 @@ const variant21 = {
           mandatory: true,
         },
       },
-      // {
-      //   name: 'field4',
-      //   display: 'Field 4',
-      //   visible: false,
-      //   sortable: false,
-      //   type: 'string',
-      //   filter: {
-      //     type: 'autocomplete',
-      //     dynamicOptions: {
-      //       minimumLength: 3,
-      //       returnAsStaticOptions: true,
-      //     },
-      //     staticOptions: [
-      //       { name: 'Fezzick', display: 'Fezzick' },
-      //       { name: 'Inigo Montoya', display: 'Inigo Montoya' },
-      //       { name: 'Prince Humperdink', display: 'Prince Humperdink' },
-      //       { name: 'Princess Buttercup', display: 'Princess Buttercup' },
-      //       { name: 'Westley', display: 'Westley' },
-      //     ],
-      //     mandatory: true,
-      //   },
-      // },
-      // {
-      //   name: 'field5',
-      //   display: 'Field 5',
-      //   sortable: false,
-      //   type: 'string',
-      //   mandatory: false,
-      //   visible: false,
-      //   filter: {
-      //     type: 'autocomplete',
-      //     dynamicOptions: {
-      //       minimumLength: 3,
-      //       returnAsStaticOptions: false,
-      //     },
-      //     mandatory: true,
-      //   },
-      // },
+      {
+        name: 'field4',
+        display: 'Field 4',
+        visible: false,
+        sortable: false,
+        type: 'string',
+        filter: {
+          type: 'autocomplete',
+          dynamicOptions: {
+            minimumLength: 3,
+            returnAsStaticOptions: true,
+          },
+          staticOptions: [
+            { name: 'Fezzick', display: 'Fezzick' },
+            { name: 'Inigo Montoya', display: 'Inigo Montoya' },
+            { name: 'Prince Humperdink', display: 'Prince Humperdink' },
+            { name: 'Princess Buttercup', display: 'Princess Buttercup' },
+            { name: 'Westley', display: 'Westley' },
+          ],
+          mandatory: true,
+        },
+      },
+      {
+        name: 'field5',
+        display: 'Field 5',
+        sortable: false,
+        type: 'string',
+        mandatory: false,
+        visible: false,
+        filter: {
+          type: 'autocomplete',
+          dynamicOptions: {
+            minimumLength: 3,
+            returnAsStaticOptions: false,
+          },
+          mandatory: true,
+        },
+      },
       {
         name: 'field6',
         display: 'Field 6',

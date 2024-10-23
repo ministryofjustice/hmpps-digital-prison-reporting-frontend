370405cf60cf0862d2737552ac6f6b68d4adaa31
rossdowthwaite <rossdowthwaite@gmail.com>
2024-10-23 11:17:29 +0100

    statusHelper tests to include dashboards

--- src/dpr/utils/requestStatusHelper.test.ts
+++ src/dpr/utils/requestStatusHelper.test.ts
@@ -4,6 +4,7 @@ import { ReportType, RequestStatus } from '../types/UserReports'
 import { Services } from '../types/Services'
 import * as ReportStatusHelper from './requestStatusHelper'
 import MetricService from '../services/metricsService'
+import DashboardService from '../services/dashboardService'
 
 describe('ReportStatusHelper', () => {
   const services: Services = {
@@ -43,6 +44,24 @@ describe('ReportStatusHelper', () => {
       expect(result.status).toEqual(RequestStatus.FINISHED)
     })
 
+    it('should get the request dashboard status', async () => {
+      services.dashboardService = {
+        getAsyncStatus: jest.fn().mockResolvedValue({ status: RequestStatus.FINISHED }),
+      } as unknown as DashboardService
+
+      const req = {
+        body: {
+          status: RequestStatus.STARTED,
+          requestedAt: new Date(),
+          type: ReportType.DASHBOARD,
+        },
+      } as unknown as Request
+
+      const result = await ReportStatusHelper.getStatus({ req, res, services })
+
+      expect(result.status).toEqual(RequestStatus.FINISHED)
+    })
+
     it('should get the request status as failed', async () => {
       services.reportingService = {
         getAsyncReportStatus: jest.fn().mockResolvedValue({ status: RequestStatus.FAILED, error: 'Error Message' }),
@@ -53,6 +72,25 @@ describe('ReportStatusHelper', () => {
           status: RequestStatus.STARTED,
           requestedAt: new Date(),
           type: ReportType.REPORT,
+        },
+      } as unknown as Request
+
+      const result = await ReportStatusHelper.getStatus({ req, res, services })
+
+      expect(result.status).toEqual(RequestStatus.FAILED)
+      expect(result.errorMessage).toEqual({ developerMessage: 'Error Message' })
+    })
+
+    it('should get the request dashboard status as failed', async () => {
+      services.dashboardService = {
+        getAsyncStatus: jest.fn().mockResolvedValue({ status: RequestStatus.FAILED, error: 'Error Message' }),
+      } as unknown as DashboardService
+
+      const req = {
+        body: {
+          status: RequestStatus.STARTED,
+          requestedAt: new Date(),
+          type: ReportType.DASHBOARD,
         },
       } as unknown as Request
 
@@ -80,6 +118,24 @@ describe('ReportStatusHelper', () => {
       expect(result.status).toEqual(RequestStatus.EXPIRED)
     })
 
+    it('should get the request dashboard status as expired', async () => {
+      services.dashboardService = {
+        getAsyncStatus: jest.fn().mockRejectedValue(new Error('Async error')),
+      } as unknown as DashboardService
+
+      const req = {
+        body: {
+          status: RequestStatus.FINISHED,
+          requestedAt: new Date(),
+          type: ReportType.DASHBOARD,
+        },
+      } as unknown as Request
+
+      const result = await ReportStatusHelper.getStatus({ req, res, services })
+
+      expect(result.status).toEqual(RequestStatus.EXPIRED)
+    })
+
     it('should timeout the request and return status as failed', async () => {
       services.reportingService = {
         getAsyncReportStatus: jest.fn().mockResolvedValue({ status: RequestStatus.STARTED }),
@@ -93,6 +149,28 @@ describe('ReportStatusHelper', () => {
           status: RequestStatus.STARTED,
           requestedAt,
           type: ReportType.REPORT,
+        },
+      } as unknown as Request
+
+      const result = await ReportStatusHelper.getStatus({ req, res, services })
+
+      expect(result.status).toEqual(RequestStatus.FAILED)
+      expect(result.errorMessage).toEqual({ userMessage: 'Request taking too long. Request Halted' })
+    })
+
+    it('should timeout the dashboard request and return status as failed', async () => {
+      services.dashboardService = {
+        getAsyncStatus: jest.fn().mockResolvedValue({ status: RequestStatus.STARTED }),
+      } as unknown as DashboardService
+
+      const requestedAt = new Date()
+      requestedAt.setMinutes(requestedAt.getMinutes() - 20)
+
+      const req = {
+        body: {
+          status: RequestStatus.STARTED,
+          requestedAt,
+          type: ReportType.DASHBOARD,
         },
       } as unknown as Request
 
@@ -124,10 +202,49 @@ describe('ReportStatusHelper', () => {
       expect(result.executionId).toEqual('executionId')
     })
 
-    it('should notget status as expired', async () => {
+    it('should get dashboard status as expired', async () => {
+      services.dashboardService = {
+        getAsyncStatus: jest.fn().mockRejectedValue(new Error('Async error')),
+      } as unknown as DashboardService
+
+      const req = {
+        body: {
+          executionId: 'executionId',
+          status: RequestStatus.READY,
+          requestedAt: new Date(),
+          type: ReportType.DASHBOARD,
+        },
+      } as unknown as Request
+
+      const result = await ReportStatusHelper.getExpiredStatus({ req, res, services })
+
+      expect(result.isExpired).toBeTruthy()
+      expect(result.executionId).toEqual('executionId')
+    })
+
+    it('should not get status as expired', async () => {
       services.reportingService = {
         getAsyncReportStatus: jest.fn().mockResolvedValue({ status: RequestStatus.FINISHED }),
       } as unknown as ReportingService
+
+      const req = {
+        body: {
+          executionId: 'executionId',
+          status: RequestStatus.FINISHED,
+          requestedAt: new Date(),
+        },
+      } as unknown as Request
+
+      const result = await ReportStatusHelper.getExpiredStatus({ req, res, services })
+
+      expect(result.isExpired).toBeFalsy()
+      expect(result.executionId).toEqual('executionId')
+    })
+
+    it('should not get dashboard status as expired', async () => {
+      services.dashboardService = {
+        getAsyncStatus: jest.fn().mockResolvedValue({ status: RequestStatus.FINISHED }),
+      } as unknown as DashboardService
 
       const req = {
         body: {

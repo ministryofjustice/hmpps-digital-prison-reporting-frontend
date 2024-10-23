be5bbd5fc150dc9a4d4b195180e39dd3075a0d0b
rossdowthwaite <rossdowthwaite@gmail.com>
2024-10-23 16:54:16 +0100

    fixed cypress tests

--- test-app/mocks/mockClients/store/mockRedisStore.js
+++ test-app/mocks/mockClients/store/mockRedisStore.js
@@ -1,5 +1,5 @@
 const mockDataV1 = require('./mockUserListDataV1')
-const mockDataV2 = require('./mockUserListDataV2')
+// const mockDataV2 = require('./mockUserListDataV2')
 
 const MockUserStoreService = class MockUserStoreService {
   constructor() {
@@ -8,14 +8,14 @@ const MockUserStoreService = class MockUserStoreService {
         mockDataV1.mockRequestedReports[0],
         mockDataV1.mockRequestedReports[1],
         mockDataV1.mockRequestedReports[2],
-        mockDataV1.mockRequestedReports[3],
+        // mockDataV1.mockRequestedReports[3],
         mockDataV1.mockRequestedReports[4],
-        mockDataV2.mockRequestedReports[0],
-        mockDataV2.mockRequestedReports[1],
-        mockDataV2.mockRequestedReports[2],
-        mockDataV2.mockRequestedDashboards[0],
-        mockDataV2.mockRequestedDashboards[1],
-        mockDataV2.mockRequestedDashboards[2],
+        // mockDataV2.mockRequestedReports[0],
+        // mockDataV2.mockRequestedReports[1],
+        // mockDataV2.mockRequestedReports[2],
+        // mockDataV2.mockRequestedDashboards[0],
+        // mockDataV2.mockRequestedDashboards[1],
+        // mockDataV2.mockRequestedDashboards[2],
       ],
       recentlyViewedReports: [mockDataV1.mockViewedReports[0], mockDataV1.mockViewedReports[1]],
       bookmarks: [{ reportId: 'test-report-1', variantId: 'variantId-1' }],

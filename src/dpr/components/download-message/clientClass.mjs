ca606b56292a836e5abc2eed7edf30b4725748f1
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-10-28 11:27:06 +0000

    replaced hash fragment for route (#261)
    App insights does not log #fragments in urls. Currently, download clicks
    are recorded by using a hash fragment therefore app insights does not
    record this event.
    
    Fixed by switching hash fragment to a route path

--- src/dpr/components/download-message/clientClass.mjs
+++ src/dpr/components/download-message/clientClass.mjs
@@ -7,7 +7,7 @@ export default class DownloadMessage extends DprClientClass {
 
   initialise() {
     this.downloadMessage = this.getElement()
-    if (window.location.hash === '#download') {
+    if (window.location.href.indexOf('download') > -1) {
       this.downloadMessage.classList.remove('dpr-download-message--hidden')
     }
   }

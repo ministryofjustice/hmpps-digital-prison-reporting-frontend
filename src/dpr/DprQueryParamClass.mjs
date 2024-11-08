8cafeeba0a27b4250452ce4cec62fdd8c1db03f8
rossdowthwaite <rossdowthwaite@gmail.com>
2024-11-08 11:39:53 +0000

    Datepicker fix

--- src/dpr/DprQueryParamClass.mjs
+++ src/dpr/DprQueryParamClass.mjs
@@ -69,8 +69,11 @@ export default class DprQueryParamClass extends DprClientClass {
       let { value } = input
 
       if (input.classList.contains('moj-js-datepicker-input')) {
-        const dateValue = dayjs(value, 'DD/MM/YYYY').format('YYYY-MM-DD')
-        value = dateValue !== 'Invalid date' ? dateValue : ''
+        let formatted = dayjs(value, 'DD/MM/YYYY').format('YYYY-MM-DD')
+        if (formatted === 'Invalid Date') {
+          formatted = dayjs(value, 'D/M/YYYY').format('YYYY-MM-DD')
+        }
+        value = formatted !== 'Invalid Date' ? formatted : ''
       }
 
       if (name) this.updateQueryParam(name, value)

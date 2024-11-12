5cc14aaf092e59ab1a13adfcfff4484e9e960b53
rossdowthwaite <rossdowthwaite@gmail.com>
2024-11-12 14:18:39 +0000

    new dahsboard definition types + mocks

--- src/dpr/types/Dashboards.ts
+++ src/dpr/types/Dashboards.ts
@@ -1,3 +1,5 @@
+import { ChartUnit, ChartType } from './Charts'
+
 export interface DashboardDefinition {
   id: string
   name: string
@@ -8,4 +10,42 @@ export interface DashboardDefinition {
 
 export interface DashboardMetricDefinition {
   id: string
+}
+
+export interface DashboardDefinitionV2 {
+  id: string
+  name: string
+  description: string
+  loadType?: 'async' | 'sync'
+  metrics: DashboardMetricDefinitionV2[]
+  dataset: DashboardDataSet
+}
+
+interface DashboardMetricDefinitionV2 {
+  id: string
+  name: string
+  display: string
+  description: string
+  charts: DashboardChartDefinition[]
+}
+
+interface DashboardChartDefinition {
+  type: ChartType
+  unit: ChartUnit
+  label: ChartColumn
+  columns: ChartColumn[]
+}
+
+interface ChartColumn {
+  name: string
+  display: string
+}
+interface DashboardDataSet {
+  id: string
+  dimension: DashboardDataSetDimension
+}
+
+interface DashboardDataSetDimension {
+  id: string
+  filter: string // TODO
 }

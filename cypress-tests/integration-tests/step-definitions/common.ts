08a8611d3ffbc9322d53506f35362c589aed6701
rossdowthwaite <rossdowthwaite@gmail.com>
2024-11-25 17:16:14 +0000

    Updated int tests

--- cypress-tests/integration-tests/step-definitions/common.ts
+++ cypress-tests/integration-tests/step-definitions/common.ts
@@ -65,7 +65,7 @@ When(/I navigate to the async-home page/, () => {
 })
 
 When(/I navigate to the async-query page/, () => {
-  cy.visit('/async-reports/test-report-1/variantId-1/request')
+  cy.visit('/async-reports/test-report-3/variantId-1/request')
 })
 
 Then(/The text (.+) is displayed on the page/, (text) => {

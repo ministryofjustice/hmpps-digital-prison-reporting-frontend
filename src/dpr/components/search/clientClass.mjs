88193c615b5d7c02ceebd85a42d555dee3f6a8cd
Ross Dowthwaite <rossdowthwaite@gmail.com>
2024-12-16 09:21:48 +0000

    toggle unauthorised reports in the catalogue (#312)
    - Unauthorised flag on restricted reports
    - Filter on reports catalogue to show/hide unauthorised reports

--- src/dpr/components/search/clientClass.mjs
+++ src/dpr/components/search/clientClass.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 /* eslint-disable no-param-reassign */
 import { DprClientClass } from '../../DprClientClass.mjs'
 
@@ -6,57 +7,88 @@ export default class Search extends DprClientClass {
     return 'search'
   }
 
-  updateSearchListing(value) {
+  initialise() {
+    this.searchBox = this.getElement().querySelector('.dpr-search-box')
+    this.shwoUnauthorisedCheckbox = this.getElement().querySelector('#show-unauthorised')
+
+    if (this.searchBox) {
+      this.initSeachBoxEvents()
+    }
+
+    if (this.shwoUnauthorisedCheckbox) {
+      this.initShowUnauthorisedEvents()
+    }
+
+    this.initInputFromQueryParams()
+  }
+
+  initSeachBoxEvents() {
+    this.searchBox.addEventListener('keyup', (e) => {
+      this.updateTableRows(e.target.value)
+
+      // Update Query Params
+      const queryParams = new URLSearchParams(window.location.search)
+      queryParams.set(this.searchBox.id, e.target.value)
+      window.history.replaceState(null, null, `?${queryParams.toString()}`)
+    })
+  }
+
+  initShowUnauthorisedEvents() {
+    this.shwoUnauthorisedCheckbox.addEventListener('change', (e) => {
+      const queryParams = new URLSearchParams(window.location.search)
+      if (e.target.checked) {
+        queryParams.set(e.target.id, e.target.value)
+      } else {
+        queryParams.delete(e.target.id, e.target.value)
+      }
+      window.history.replaceState(null, null, `?${queryParams.toString()}`)
+
+      this.updateTableRows(this.searchBox.value)
+    })
+  }
+
+  updateTableRows(searchValue) {
     const table = this.getElement().querySelector('table').querySelector('tbody')
 
     const rows = Array.from(table.rows)
     rows.forEach((row) => row.classList.add('search-option-hide'))
 
-    const searchValue = value.toLowerCase()
+    this.updateSearchListing(rows, searchValue)
+    this.toggleUnauthorisedRows(rows)
+  }
 
+  toggleUnauthorisedRows(rows) {
+    const queryParams = new URLSearchParams(window.location.search)
+    const value = queryParams.get('show-unauthorised')
+
+    rows
+      .filter((row) => {
+        return Array.from(row.cells).find((cell) => {
+          return cell.innerHTML.includes('dpr-unauthorised-report')
+        })
+      })
+      .forEach((row) => {
+        if (value) {
+          row.classList.remove('search-option-hide')
+        } else if (!row.classList.contains('search-option-hide')) row.classList.add('search-option-hide')
+      })
+  }
+
+  updateSearchListing(rows, value) {
     rows
       .filter((row) => {
         return (
           !value ||
           value.length === 0 ||
           Array.from(row.cells).find((cell) => {
+            const searchValue = value.toLowerCase()
             return cell.innerText.toLowerCase().includes(searchValue.toLowerCase())
           })
         )
       })
       .forEach((row) => {
-        // NOTE: Text highlighting functionality below breaks the client class JS. Causing bookmarking & showmore to break
-
-        // Array.from(row.cells).forEach((cell) => {
-        //   const innerHTMLContent = cell.innerHTML
-        //   cell.innerHTML = innerHTMLContent.replace(/<\/?b>/gi, '')
-
-        //   if (searchValue) {
-        //     const innerHTMLText = cell.innerText
-        //     const text = innerHTMLText.replaceAll(new RegExp(`(${searchValue})`, 'gi'), `<b>$1</b>`)
-        //     const innerHTMLCont = cell.innerHTML
-        //     cell.innerHTML = innerHTMLCont.replace(cell.innerText, text)
-        //   }
-        // })
         row.classList.remove('search-option-hide')
       })
-  }
-
-  initialise() {
-    const searchBox = this.getElement().querySelector('.dpr-search-box')
-
-    if (searchBox) {
-      searchBox.addEventListener('keyup', (e) => {
-        this.updateSearchListing(e.target.value)
-
-        // Update Query Params
-        const queryParams = new URLSearchParams(window.location.search)
-        queryParams.set(searchBox.id, e.target.value)
-        window.history.replaceState(null, null, `?${queryParams.toString()}`)
-      })
-
-      this.initInputFromQueryParams()
-    }
   }
 
   // eslint-disable-next-line
@@ -65,9 +97,14 @@ export default class Search extends DprClientClass {
 
     urlParams.forEach((value, key) => {
       const element = document.getElementById(key)
+
       if (element && element.classList.contains('dpr-search-box')) {
         element.value = value
-        this.updateSearchListing(element.value)
+        this.updateTableRows(element.value)
+      }
+
+      if (element && element.id === 'show-unauthorised') {
+        element.setAttribute('checked', '')
       }
     })
   }

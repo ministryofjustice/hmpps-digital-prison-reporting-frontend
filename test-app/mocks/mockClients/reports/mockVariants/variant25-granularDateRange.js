1f7f25a05d85e26706f7485621bd6e2bd1df1f29
rossdowthwaite <rossdowthwaite@gmail.com>
2024-12-19 10:08:50 +0000

    init mock variant with filter type

---
+++ test-app/mocks/mockClients/reports/mockVariants/variant25-granularDateRange.js
@@ -0,0 +1,31 @@
+const variant25 = {
+  id: 'variantId-25',
+  name: 'Granular Daterange',
+  description: 'Granular Daterange variant',
+  resourceName: 'reports/list',
+  classification: 'OFFICIAL',
+  printable: false,
+  specification: {
+    template: 'list',
+    fields: [
+      {
+        name: 'field1',
+        display: 'Field 1',
+        sortable: false,
+        visible: true,
+        type: 'date',
+        mandatory: false,
+        filter: {
+          type: 'granular-daterange',
+          defaultValue: '2024-12-19 - 2024-11-19',
+          mandatory: true,
+          defaultGranularity: 'monthly',
+          min: '2003-02-01',
+          max: '2007-05-04',
+        },
+      },
+    ],
+  },
+}
+
+module.exports = variant25

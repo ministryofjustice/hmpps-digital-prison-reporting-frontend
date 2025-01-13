1dcce32204fddc9db3474f503c22b9a69b91a383
rossdowthwaite <rossdowthwaite@gmail.com>
2025-01-13 09:30:32 +0000

    unit tests for future quick filtes

--- src/dpr/components/_inputs/granular-date-range/utils.test.ts
+++ src/dpr/components/_inputs/granular-date-range/utils.test.ts
@@ -22,7 +22,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2024-06-05',
+        start: '2024-06-06',
         end: '2024-06-06',
         granularity: {
           value: 'daily',
@@ -44,7 +44,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2024-05-30',
+        start: '2024-05-31',
         end: '2024-06-06',
         granularity: {
           value: 'daily',
@@ -66,7 +66,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2024-05-06',
+        start: '2024-05-07',
         end: '2024-06-06',
         granularity: {
           value: 'daily',
@@ -88,7 +88,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2024-05-06',
+        start: '2024-05-07',
         end: '2024-06-06',
         granularity: {
           value: 'monthly',
@@ -110,7 +110,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2024-04-30',
+        start: '2024-05-01',
         end: '2024-05-31',
         granularity: {
           value: 'monthly',
@@ -132,7 +132,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2024-03-06',
+        start: '2024-03-07',
         end: '2024-06-06',
         granularity: {
           value: 'daily',
@@ -154,7 +154,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2024-03-06',
+        start: '2024-03-07',
         end: '2024-06-06',
         granularity: {
           value: 'monthly',
@@ -176,7 +176,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2024-02-29',
+        start: '2024-03-01',
         end: '2024-05-31',
         granularity: {
           value: 'monthly',
@@ -198,7 +198,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2023-06-06',
+        start: '2023-06-07',
         end: '2024-06-06',
         granularity: {
           value: 'annually',
@@ -220,7 +220,7 @@ describe('GranularDatePickerUtils', () => {
       const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
 
       expect(result.value).toEqual({
-        start: '2022-12-31',
+        start: '2023-01-01',
         end: '2023-12-31',
         granularity: {
           value: 'annually',
@@ -229,6 +229,226 @@ describe('GranularDatePickerUtils', () => {
         quickFilter: {
           value: 'last-full-year',
           display: 'Last full year',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - tomorrow', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'tomorrow',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2024-06-07',
+        end: '2024-06-07',
+        granularity: {
+          value: 'daily',
+          display: 'Daily',
+        },
+        quickFilter: {
+          value: 'tomorrow',
+          display: 'Tomorrow',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - next-seven-days', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'next-seven-days',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2024-06-06',
+        end: '2024-06-12',
+        granularity: {
+          value: 'daily',
+          display: 'Daily',
+        },
+        quickFilter: {
+          value: 'next-seven-days',
+          display: 'Next 7 days',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - next-thirty-days', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'next-thirty-days',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2024-06-06',
+        end: '2024-07-05',
+        granularity: {
+          value: 'daily',
+          display: 'Daily',
+        },
+        quickFilter: {
+          value: 'next-thirty-days',
+          display: 'Next 30 days',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - next-month', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'next-month',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2024-06-06',
+        end: '2024-07-05',
+        granularity: {
+          value: 'monthly',
+          display: 'Monthly',
+        },
+        quickFilter: {
+          value: 'next-month',
+          display: 'Next month',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - next-full-month', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'next-full-month',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2024-07-01',
+        end: '2024-07-31',
+        granularity: {
+          value: 'monthly',
+          display: 'Monthly',
+        },
+        quickFilter: {
+          value: 'next-full-month',
+          display: 'Next full month',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - next-90-days', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'next-90-days',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2024-06-06',
+        end: '2024-09-05',
+        granularity: {
+          value: 'daily',
+          display: 'Daily',
+        },
+        quickFilter: {
+          value: 'next-90-days',
+          display: 'Next 90 days',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - next-3-months', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'next-3-months',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2024-06-06',
+        end: '2024-09-05',
+        granularity: {
+          value: 'monthly',
+          display: 'Monthly',
+        },
+        quickFilter: {
+          value: 'next-3-months',
+          display: 'Next 3 months',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - next-full-3-months', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'next-full-3-months',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2024-07-01',
+        end: '2024-09-30',
+        granularity: {
+          value: 'monthly',
+          display: 'Monthly',
+        },
+        quickFilter: {
+          value: 'next-full-3-months',
+          display: 'Next full 3 months',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - next-year', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'next-year',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2024-06-06',
+        end: '2025-06-05',
+        granularity: {
+          value: 'annually',
+          display: 'Annually',
+        },
+        quickFilter: {
+          value: 'next-year',
+          display: 'Next year',
+        },
+      })
+    })
+
+    it('should get the filter from the definition and set the value - next-full-year', () => {
+      const filter = {
+        type: 'granulardaterange',
+        defaultValue: 'next-full-year',
+      } as unknown as components['schemas']['FilterDefinition'] & { defaultGranularity: string }
+
+      const result = GranularDatePickerUtils.getFilterFromDefinition(filter, {} as unknown as FilterValue)
+
+      expect(result.value).toEqual({
+        start: '2025-01-01',
+        end: '2025-12-31',
+        granularity: {
+          value: 'annually',
+          display: 'Annually',
+        },
+        quickFilter: {
+          value: 'next-full-year',
+          display: 'Next full year',
         },
       })
     })

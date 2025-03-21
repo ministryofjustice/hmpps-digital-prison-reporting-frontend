6d36e43e331b342957cbfb1cd05f34f305bb7be7
rossdowthwaite <rossdowthwaite@gmail.com>
2025-03-21 14:37:31 +0000

    gulp package static libs

--- src/dpr/middleware/setupDprResources.ts
+++ src/dpr/middleware/setupDprResources.ts
@@ -26,26 +26,6 @@ export default function setDPRResources(config: Dict<string>): Router {
       path: '/assets/govuk/all.js',
       location: '/node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js',
     },
-    {
-      path: '/assets/ext/jquery.min.js',
-      location: '/node_modules/jquery/dist/jquery.min.js',
-    },
-    {
-      path: '/assets/ext/chart.js',
-      location: '/node_modules/chart.js/dist/chart.umd.js',
-    },
-    {
-      path: '/assets/ext/chartjs-datalabels.js',
-      location: '/node_modules/chartjs-plugin-datalabels/dist/chartjs-plugin-datalabels.min.js',
-    },
-    {
-      path: '/assets/ext/day.js',
-      location: '/node_modules/dayjs/dayjs.min.js',
-    },
-    {
-      path: '/assets/ext/dayjs/plugin/customParseFormat.js',
-      location: '/node_modules/dayjs/plugin/customParseFormat.js',
-    },
   ]
 
   staticResources.forEach((r) => {

5ba87f8f0f1d1ddf45537cc940cc1a8235ec88b9
rossdowthwaite <rossdowthwaite@gmail.com>
2025-05-08 11:05:11 +0100

    fixed int tests

--- cypress-tests/integration-tests/step-definitions/recently-viewed.ts
+++ cypress-tests/integration-tests/step-definitions/recently-viewed.ts
@@ -7,12 +7,10 @@ Then('I click on the recently viewed reports tab', () => {
 })
 
 Then('the viewed reports are displayed correctly', () => {
-  new AsyncHomePage().viewedReportsList().find('tr').should('have.length', 8)
+  new AsyncHomePage().viewedReportsList().find('tr').should('have.length', 6)
 })
 
 Then('the status and timestamp is displayed for each viewed report', () => {
-  new AsyncHomePage().viewedReportRow_Finished().parent().parent().parent().parent().contains('READY').should('exist')
-  new AsyncHomePage().viewedReportRow_Expired().parent().parent().parent().parent().contains('EXPIRED').should('exist')
   new AsyncHomePage().viewedReportRow_FinishedV2().parent().parent().parent().parent().contains('READY').should('exist')
   new AsyncHomePage()
     .viewedReportRow_ExpiredV2()
@@ -37,17 +35,6 @@ Then(
   /^I click on a ready viewed (report|reportV2|dashboard|interactive report)$/,
   function (this: Mocha.Context, reportType: string) {
     if (reportType === 'report') {
-      new AsyncHomePage()
-        .viewedReportRow_Finished()
-        .parent()
-        .parent()
-        .parent()
-        .parent()
-        .contains('Go to report')
-        .click()
-    }
-
-    if (reportType === 'reportV2') {
       new AsyncHomePage()
         .viewedReportRow_FinishedV2()
         .parent()
@@ -86,10 +73,6 @@ Then(
   /^I click on the (Remove|Refresh) button of an expired viewed (report|reportV2|dashboard)$/,
   function (this: Mocha.Context, action: string, reportType: string) {
     if (reportType === 'report') {
-      new AsyncHomePage().viewedReportRow_Expired().parent().parent().parent().parent().contains(action).click()
-    }
-
-    if (reportType === 'reportV2') {
       new AsyncHomePage().viewedReportRow_ExpiredV2().parent().parent().parent().parent().contains(action).click()
     }
 
@@ -106,10 +89,6 @@ Then(
       new AsyncHomePage().viewedReportRow_Expired().should('not.exist')
     }
 
-    if (reportType === 'reportV2') {
-      new AsyncHomePage().viewedReportRow_ExpiredV2().should('not.exist')
-    }
-
     if (reportType === 'dashboard') {
       new AsyncHomePage().viewedDashboardRow_Expired().should('not.exist')
     }
@@ -120,13 +99,6 @@ Then(
   /^I am taken to the query page for the viewed (report|reportV2|dashboard)$/,
   function (this: Mocha.Context, reportType: string) {
     if (reportType === 'report') {
-      cy.url().should(
-        'eq',
-        'http://localhost:3010/async/report/test-report-3/variantId-1/request?filters.field1=value1.3&filters.field2=value2.3&filters.field3.start=2003-09-05&filters.field3.end=2007-05-01&filters.field7=2007-05-04&sortColumn=field1&sortedAsc=true&filters.field4=Inigo+Montoya&filters.field8=value8.2&filters.field8=value8.3',
-      )
-    }
-
-    if (reportType === 'reportV2') {
       cy.url().should(
         'eq',
         'http://localhost:3010/async/report/test-report-3/variantId-1/request?filters.field2=value2.3&filters.field3.start=2003-09-05&filters.field3.end=2007-05-01&filters.field7=2007-05-04&sortColumn=field1&sortedAsc=true&filters.field4=Inigo+Montoya&filters.field1=value1.2&filters.field8=value8.2&filters.field8=value8.3',
@@ -144,13 +116,6 @@ Then(/^I am taken to the async (report|reportV2|dashboard)$/, function (this: Mo
     cy.url().should(
       'eq',
       'http://localhost:3010/async/report/test-report-3/variantId-1/request/tblId_1729766362362/report?columns=field1&columns=field2&columns=field3&columns=field6&columns=field7&pageSize=10&selectedPage=10',
-    )
-  }
-
-  if (reportType === 'reportV2') {
-    cy.url().should(
-      'eq',
-      'http://localhost:3010/async/report/test-report-3/variantId-1/request/tblId_1729766362362/report?columns=field1&columns=field2&columns=field3&columns=field6&columns=field7',
     )
   }
 

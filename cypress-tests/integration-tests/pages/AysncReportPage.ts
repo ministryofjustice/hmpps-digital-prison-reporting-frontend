f7583fd803703dcd30a297db6815bc2d9840f6ce
rossdowthwaite <rossdowthwaite@gmail.com>
2025-06-06 16:35:06 +0100

    Fix int tests

--- cypress-tests/integration-tests/pages/AysncReportPage.ts
+++ cypress-tests/integration-tests/pages/AysncReportPage.ts
@@ -2,7 +2,7 @@ export type PageElement = Cypress.Chainable<JQuery>
 
 export default class QueryPage {
   columnsDetails = (): PageElement =>
-    cy.xpath(`//*[@id="main-content"]/div/div/div[1]/div/div[4]/div/div[2]/div/details/summary`)
+    cy.xpath(`//*[@id="main-content"]/div/div/div[1]/div/div[5]/div/div[2]/div/details/summary`)
 
   column4CheckBox = (): PageElement => cy.xpath(`//*[@id="columns-4"]`)
 
@@ -11,10 +11,10 @@ export default class QueryPage {
   column7CheckBox = (): PageElement => cy.xpath(`//*[@id="columns-7"]`)
 
   applyColumnsButton = (): PageElement =>
-    cy.xpath(`//*[@id="main-content"]/div/div/div[1]/div/div[4]/div/div[2]/div/details/div/form/div[2]/button`)
+    cy.xpath(`//*[@id="main-content"]/div/div/div[1]/div/div[5]/div[1]/div[2]/div/details/div/form/div[2]/button`)
 
   resetColumns = (): PageElement =>
-    cy.xpath(`//*[@id="main-content"]/div/div/div[1]/div/div[4]/div/div[2]/div/details/div/form/div[2]/a`)
+    cy.xpath(`//*[@id="main-content"]/div/div/div[1]/div/div[5]/div[1]/div[2]/div/details/div/form/div[2]/a`)
 
   pageSizeSelector = (): PageElement => cy.xpath(`//*[@id="page-size-select"]`)
 

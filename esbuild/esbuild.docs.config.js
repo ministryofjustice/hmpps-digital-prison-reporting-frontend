7274b25ab53cb90240df5d12436be6a4b87095ee
James Reed <james.reed2@justice.gov.uk>
2025-07-01 10:12:50 +0100

    step towards making prod builds work

--- esbuild/esbuild.docs.config.js
+++ esbuild/esbuild.docs.config.js
@@ -57,25 +57,28 @@ const buildConfig = {
 }
 
 const buildLibrary = async () => {
-  const scssFiles = glob.sync([
-      'docs/scss/local-paths.scss',
-      'docs/scss/base.scss',
-      'docs/scss/tabs.scss',
-      'docs/scss/example.scss',
-      'src/dpr/components/**/*.scss',])
-  const imports = scssFiles.map(file => `@import '${file}';`).join('\n')
+    const args = process.argv
+    const scssFiles = glob.sync([
+        args.includes('--local') ? 'docs/scss/local-paths.scss' : 'docs/scss/remote-paths.scss',
+        'docs/scss/base.scss',
+        'docs/scss/tabs.scss',
+        'docs/scss/example.scss',
+        'src/dpr/components/**/*.scss',])
+    const imports = scssFiles.map(file => `@import '${file}';`).join('\n')
 
-  const allImportsBundle = 'esbuild/all-imports-bundle.scss'
-  fs.writeFileSync(allImportsBundle, imports, {
-    flush: true
-  })
-  
-  // if (args.includes('--build')) {
-  await buildApp(buildConfig)
-  await buildAssets(buildConfig)
-  execSync(`eleventy --input=${path.join(__dirname, "../docs")} --output=${path.join(__dirname, "../dist-docs/dpr")}`, {
-    stdio: 'inherit'
-  })
+    const allImportsBundle = 'esbuild/all-imports-bundle.scss'
+    fs.writeFileSync(allImportsBundle, imports, {
+        flush: true
+    })
+    
+    // if (args.includes('--build')) {
+    await buildApp(buildConfig)
+    await buildAssets(buildConfig)
+    const pathPrefixArg = args.includes('--local') ? '' : '--pathprefix hmpps-digital-prison-reporting-frontend'
+    console.log(`eleventy ${pathPrefixArg} --input=${path.join(__dirname, "../docs")} --output=${path.join(__dirname, "../dist-docs/dpr")}`)
+    execSync(`eleventy ${pathPrefixArg} --input=${path.join(__dirname, "../docs")} --output=${path.join(__dirname, "../dist-docs/dpr")}`, {
+        stdio: 'inherit'
+    })
 }
 
 const main = async () => {
---
+++ package/README.md
@@ -0,0 +1,5 @@
+# HMPPS Digital Prison Reporting Front-end Library
+
+A front-end library for components created by the Ministry of Justice Digital Prison Reporting team.
+
+To integrate the components into your project, please see the consumer documentation here: https://ministryofjustice.github.io/hmpps-digital-prison-reporting-frontend/

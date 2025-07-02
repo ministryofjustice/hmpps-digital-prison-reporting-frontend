522803d22ca464fb66700b202c4010cc2b054193
rossdowthwaite <rossdowthwaite@gmail.com>
2025-07-02 17:43:23 +0100

    fix test

--- src/dpr/routes/journeys/my-reports/requested-reports/service.test.ts
+++ src/dpr/routes/journeys/my-reports/requested-reports/service.test.ts
@@ -253,8 +253,8 @@ describe('RequestedReportService', () => {
         url: {
           ...MockRequestedListData2.requestedReady.url,
           report: {
-            fullUrl: `http://localhost:3010/dpr/view-report/async/report/${reportId}/${id}/${tableId}/report/`,
-            pathname: `/dpr/view-report/async/report/${reportId}/${id}/${tableId}/report/`,
+            fullUrl: `http://localhost:3010/dpr/view-report/async/report/${reportId}/${id}/${tableId}/report`,
+            pathname: `/dpr/view-report/async/report/${reportId}/${id}/${tableId}/report`,
           },
         },
       }

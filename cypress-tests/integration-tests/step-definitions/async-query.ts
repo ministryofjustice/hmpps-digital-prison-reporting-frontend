f6535ad111fbe01a0023871d54f83694fb2b4e4a
James Reed <james.reed1@justice.gov.uk>
2025-07-14 12:54:41 +0100

    fix last url

--- cypress-tests/integration-tests/step-definitions/async-query.ts
+++ cypress-tests/integration-tests/step-definitions/async-query.ts
@@ -23,7 +23,7 @@ Then('the url is initialised to the default values', () => {
 Then('the url is set to the default values', () => {
   cy.url().should(
     'eq',
-    'http://localhost:3010/dpr/request-report/report/test-report-3/variantId-1/filters?sortColumn=field2&sortedAsc=true&filters.field1=value1.2&filters.field3.start=2003-02-01&filters.field3.end=2006-05-04&filters.field7=2005-02-01&filters.field8=value8.2&filters.field8=value8.3',
+    'http://localhost:3010/dpr/request-report/report/test-report-3/variantId-1/filters?sortColumn=field2&sortedAsc=false&filters.field1=value1.2&filters.field3.start=2003-02-01&filters.field3.end=2006-05-04&filters.field7=2005-02-01&filters.field8=value8.2&filters.field8=value8.3',
   )
 })
 

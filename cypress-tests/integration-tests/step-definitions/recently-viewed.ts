51f1f45d2f2d91d9e0801d5f17190e6e787d292b
rossdowthwaite <4712683+rossdowthwaite@users.noreply.github.com>
2025-08-04 08:19:31 +0000

    Linting fixes

--- cypress-tests/integration-tests/step-definitions/recently-viewed.ts
+++ cypress-tests/integration-tests/step-definitions/recently-viewed.ts
@@ -106,7 +106,10 @@ Then(
     }
 
     if (reportType === 'dashboard') {
-      cy.url().should('eq', 'http://localhost:3010/dpr/request-report/dashboard/mock-dashboards/test-dashboard-8/filters')
+      cy.url().should(
+        'eq',
+        'http://localhost:3010/dpr/request-report/dashboard/mock-dashboards/test-dashboard-8/filters',
+      )
     }
   },
 )

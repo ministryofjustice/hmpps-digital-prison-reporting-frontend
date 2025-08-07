068d30f40a1964c80e4fd885e3aa177c8190ec1f
rossdowthwaite <rossdowthwaite@gmail.com>
2025-08-07 09:15:00 +0100

    pr linting

--- cypress-tests/integration-tests/pages/AsyncQueryPage.ts
+++ cypress-tests/integration-tests/pages/AsyncQueryPage.ts
@@ -14,25 +14,4 @@ export default class QueryPage {
   resetFiltersButton = (): PageElement => cy.xpath(`//*[@id="async-request-reset-filters-button"]`)
 
   submitFiltersButton = (): PageElement => cy.xpath(`//*[@id="async-request-report-button"]`)
-
-  // Selected filters ----
-
-  resetSelectedFiltersButton = (): PageElement => cy.get(`#async-request-reset-filters-button`)
-
-  // DPD default selected filters
-  defaultSelectedFilter_field1 = (): PageElement => cy.get(`#dpr-selected-filters > a:nth-child(1)`)
-
-  defaultSelectedFilter_startDate = (): PageElement => cy.get(`#dpr-selected-filters > a:nth-child(2)`)
-
-  defaultSelectedFilter_endDate = (): PageElement => cy.get(`#dpr-selected-filters > a:nth-child(3)`)
-
-  defaultSelectedFilter_field7 = (): PageElement => cy.get(`#dpr-selected-filters > a:nth-child(4)`)
-
-  defaultSelectedFilter_field8 = (): PageElement => cy.get(`#dpr-selected-filters > a:nth-child(5)`)
-
-  // Save defaults ----
-
-  saveFiltersAsDefaultsButton = (): PageElement => cy.get(`#dpr-save-user-defaults`)
-
-  deleteFiltersAsDefaultsButton = (): PageElement => cy.get(`#dpr-remove-user-defaults`)
 }

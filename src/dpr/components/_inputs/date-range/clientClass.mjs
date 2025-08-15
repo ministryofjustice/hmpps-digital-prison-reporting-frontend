c810dedc089b4a165849a59fa6c4d82b71f65629
rossdowthwaite <rossdowthwaite@gmail.com>
2025-08-15 14:11:43 +0100

    Pr linting

--- src/dpr/components/_inputs/date-range/clientClass.mjs
+++ src/dpr/components/_inputs/date-range/clientClass.mjs
@@ -71,7 +71,6 @@ export default class DateRangeInput extends DprClientClass {
       durationRadioButton.addEventListener('click', (e) => {
         const durationValue = e.target.value
         this.durationValue = durationValue
-        console.log(this.durationValue)
         this.updateInputs(durationValue)
         this.removeRequiredFromDatePickers()
       })

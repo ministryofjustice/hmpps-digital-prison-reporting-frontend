19391b1146cb836af8b9fa1e2ed23e0878268f34
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2025-08-15 12:06:36 +0100

    wire in new endpoint, add tests, use wiremock, add testing-library extensions (#474)
    Co-authored-by: James Reed <james.reed1@justice.gov.uk>

--- src/dpr/routes/journeys/request-missing-report/form/tests.cy.ts
+++ src/dpr/routes/journeys/request-missing-report/form/tests.cy.ts
@@ -1,9 +1,32 @@
 context('Request missing report', () => {
   const path = '/embedded/platform/dpr/request-missing-report/feature-testing/feature-testing-missing-1/form'
 
+  beforeEach(() => {
+    cy.task('reset')
+  })
+
   it('is accessible', () => {
     cy.visit(path)
     cy.injectAxe()
     cy.checkA11y()
+  })
+
+  it('should show the form, and on successful submission, redirect to the success submitted page', () => {
+    cy.task('stubSubmitMissingRequest')
+    cy.task('stubGetDefinition')
+    cy.visit(path)
+    cy.findByRole('heading', { name: /Request a missing report/ }).should('be.visible')
+    cy.findByRole('textbox').type('a reason')
+    cy.findByRole('button', { name: /Submit request/ }).click()
+    cy.findByRole('heading', { name: /Request submitted/ }).should('be.visible')
+  })
+
+  it('should show the form, and on failed submission, redirect to the error page', () => {
+    cy.task('stubGetDefinition')
+    cy.visit(path)
+    cy.findByRole('heading', { name: /Request a missing report/ }).should('be.visible')
+    cy.findByRole('textbox').type('a reason')
+    cy.findByRole('button', { name: /Submit request/ }).click()
+    cy.findByRole('heading', { name: /Sorry, there is a problem with the service/ }).should('be.visible')
   })
 })

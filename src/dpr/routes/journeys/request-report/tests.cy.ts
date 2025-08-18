f7729fe59589053a69e54620ddc2cb8bcdc1b247
rossdowthwaite <rossdowthwaite@gmail.com>
2025-08-18 13:52:36 +0100

    Updated requested report with findByRole

--- src/dpr/routes/journeys/request-report/tests.cy.ts
+++ src/dpr/routes/journeys/request-report/tests.cy.ts
@@ -1,11 +1,5 @@
 context('Requesting a report', () => {
   const path = '/embedded/platform/'
-  const successfulRequestUrlSelector =
-    '#dpr-reports-catalogue > tbody > tr > td > a[href="dpr/request-report/report/request-examples/request-example-success/filters"]'
-  const failedRequestUrlSelector =
-    '#dpr-reports-catalogue > tbody > tr > td > a[href="dpr/request-report/report/request-examples/request-example-fail-code/filters"]'
-  const expiredRequestUrlSelector =
-    '#dpr-reports-catalogue > tbody > tr > td > a[href="dpr/request-report/report/request-examples/request-example-expire/filters"]'
 
   beforeEach(() => {
     cy.visit(path)
@@ -19,17 +13,27 @@ context('Requesting a report', () => {
 
   const checkStatuses = () => {
     cy.wait(500)
-    cy.get('#async-request-polling-status > p:nth-child(1) > .govuk-tag').contains('SUBMITTED')
+    cy.findByRole('strong').contains('SUBMITTED')
     cy.wait(1000)
-    cy.get('#async-request-polling-status > p:nth-child(1) > .govuk-tag').contains('PICKED')
+    cy.findByRole('strong').contains('PICKED')
     cy.wait(1000)
-    cy.get('#async-request-polling-status > p:nth-child(1) > .govuk-tag').contains('STARTED')
+    cy.findByRole('strong').contains('STARTED')
     cy.wait(1000)
   }
 
   describe('Successful request', () => {
     it('should succesfully request and load a report', () => {
-      cy.get(successfulRequestUrlSelector).click()
+      cy.findByLabelText(/Reports catalogue.*/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return (
+              element.textContent.includes('Successful Report') && element.textContent.includes('this will succeed')
+            )
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Request report' }).click()
+        })
+      })
       cy.url().should(
         'have.string',
         '/embedded/platform/dpr/request-report/report/request-examples/request-example-success/filters',
@@ -43,7 +47,7 @@ context('Requesting a report', () => {
         expect(location.search).to.contain(`sortColumn=field1&sortedAsc=false`)
       })
 
-      cy.get('#async-request-report-button').click()
+      cy.findByRole('button', { name: 'Request report' }).click()
       cy.url().should(
         'match',
         /http:\/\/localhost:3010\/embedded\/platform\/dpr\/request-report\/report\/request-examples\/request-example-success\/(.*)\/status/i,
@@ -81,7 +85,15 @@ context('Requesting a report', () => {
     let executionId: string
 
     it('should show an error page for a failed request', () => {
-      cy.get(failedRequestUrlSelector).click()
+      cy.findByLabelText(/Reports catalogue.*/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Fail with Status Error')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Request report' }).click()
+        })
+      })
       cy.url().should(
         'have.string',
         '/embedded/platform/dpr/request-report/report/request-examples/request-example-fail-code/filters',
@@ -93,21 +105,21 @@ context('Requesting a report', () => {
         expect(location.search).to.contain(`sortColumn=field1&sortedAsc=false`)
       })
 
-      cy.get('#async-request-report-button').click()
+      cy.findByRole('button', { name: 'Request report' }).click()
       cy.url().should(
         'match',
         /http:\/\/localhost:3010\/embedded\/platform\/dpr\/request-report\/report\/request-examples\/request-example-fail-code\/(.*)\/status/i,
       )
 
       cy.wait(500)
-      cy.get('#async-request-polling-status > p:nth-child(1) > .govuk-tag').contains('SUBMITTED')
+      cy.findByRole('strong').contains('SUBMITTED')
       cy.wait(1000)
-      cy.get('#async-request-polling-status > p:nth-child(1) > .govuk-tag').contains('PICKED')
+      cy.findByRole('strong').contains('PICKED')
       cy.wait(1000)
-      cy.get('#async-request-polling-status > p:nth-child(1) > .govuk-tag').contains('FAILED')
+      cy.findAllByRole('strong').eq(0).contains('FAILED')
 
-      cy.get('#async-request-polling-status > div.govuk-error-summary').should('be.visible')
-      cy.get('#retry-async-request').should('be.visible')
+      cy.findByRole('heading', { name: 'Your report has failed to generate' }).should('be.visible')
+      cy.findByRole('button', { name: 'Retry' }).should('be.visible')
 
       cy.url().then((url) => {
         failedRequestUrl = url
@@ -118,19 +130,19 @@ context('Requesting a report', () => {
 
     it('should show the full error', () => {
       cy.visit(failedRequestUrl)
-      cy.get('#request-error_full-error').should('be.visible')
+      cy.findAllByRole('group').contains('Show full error').should('be.visible')
     })
 
     it('should show the request details', () => {
       cy.visit(failedRequestUrl)
-      cy.get('#request-error_request-details > ul > li:nth-child(1)').contains('Report ID: request-examples')
-      cy.get('#request-error_request-details > ul > li:nth-child(2)').contains(`Execution ID: ${executionId}`)
-      cy.get('#request-error_request-details > ul > li:nth-child(3)').contains('Table ID: tblId_')
+      cy.findAllByRole('list').contains('Report ID: request-examples').should('be.visible')
+      cy.findAllByRole('list').contains(`Execution ID: ${executionId}`).should('be.visible')
+      cy.findAllByRole('list').contains('Table ID: tblId_').should('be.visible')
     })
 
     it('should go to filters page on retry', () => {
       cy.visit(failedRequestUrl)
-      cy.get('#retry-async-request').click()
+      cy.findByRole('button', { name: 'Retry' }).click()
       cy.url().should(
         'have.string',
         '/embedded/platform/dpr/request-report/report/request-examples/request-example-fail-code/filters',
@@ -144,21 +156,45 @@ context('Requesting a report', () => {
     })
 
     it('should show the correct link in the user reports list', () => {
-      cy.get('#tab_requested-reports-tab').click()
-      cy.get(`a[href="${failedRequestUrl}"`).contains('Retry')
-      cy.get(`a[data-execution-id="${executionId}"]`).contains('Remove')
+      cy.findByRole('tab', { name: /Requested/ }).click()
+
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Fail with Status Error')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Retry' })
+          cy.findByRole('link', { name: 'Remove' })
+        })
+      })
     })
 
     it('should go to filters page when clicking retry in the user catalogue', () => {
-      cy.get('#tab_requested-reports-tab').click()
-      cy.get(`a[href="${failedRequestUrl}"`).click()
+      cy.findByRole('tab', { name: /Requested/ }).click()
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Fail with Status Error')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Retry' }).click()
+        })
+      })
       cy.url().should('have.string', failedRequestUrl)
     })
 
     it('should show the correct status', () => {
-      cy.get('#tab_requested-reports-tab').click()
-      cy.get(`a[data-execution-id="${executionId}"]`).parents('.govuk-table__row').contains('Fail with Status Error')
-      cy.get(`a[data-execution-id="${executionId}"]`).parents('.govuk-table__row').contains('FAILED')
+      cy.findByRole('tab', { name: /Requested/ }).click()
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Fail with Status Error')
+          },
+        }).within(() => {
+          cy.findByRole('cell', { name: 'FAILED' })
+        })
+      })
     })
 
     it('should redirect the old status url to the new one', () => {
@@ -171,24 +207,49 @@ context('Requesting a report', () => {
     })
 
     it('should remove the item from the list by clicking remove', () => {
-      cy.get('#tab_requested-reports-tab').click()
-      cy.get(`a[data-execution-id="${executionId}"]`).click()
-      cy.get(`a[data-execution-id="${executionId}"]`).should('not.exist')
+      cy.findByRole('tab', { name: /Requested/ }).click()
+
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Fail with Status Error')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Remove' }).click()
+        })
+      })
+
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Fail with Status Error')
+          },
+        }).should('not.exist')
+      })
     })
   })
 
   describe('Aborted request', () => {
-    let executionId: string
     const filtersHref =
       'http://localhost:3010/embedded/platform/dpr/request-report/report/request-examples/request-example-success/filters?filters.field1=value1.2&filters.field3.start=2003-02-01&filters.field3.end=2006-05-04&filters.field7=2005-02-01&filters.field8=value8.2&filters.field8=value8.3&sortColumn=field1&sortedAsc=false'
 
     it('should be able to abort the request', () => {
-      cy.get(successfulRequestUrlSelector).click()
+      cy.findByLabelText(/Reports catalogue.*/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return (
+              element.textContent.includes('Successful Report') && element.textContent.includes('this will succeed')
+            )
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Request report' }).click()
+        })
+      })
       cy.url().should(
         'have.string',
         '/embedded/platform/dpr/request-report/report/request-examples/request-example-success/filters',
       )
-      cy.get('#async-request-report-button').click()
+      cy.findByRole('button', { name: 'Request report' }).click()
 
       cy.url().should(
         'match',
@@ -200,12 +261,9 @@ context('Requesting a report', () => {
         executionId = urlArr[urlArr.length - 2]
       })
 
-      cy.get('#cancel-async-request').should('be.visible')
-      cy.get('#cancel-async-request').click()
-      cy.get('#async-request-polling-status > p:nth-child(1) > .govuk-tag').contains('ABORTED')
-      cy.get('#retry-async-request').contains('Return to request page')
-
-      cy.get('#retry-async-request').click()
+      cy.findByRole('button', { name: /Cancel/ }).click()
+      cy.findByRole('strong').contains('ABORTED')
+      cy.findByRole('button', { name: /Return to request page/ }).click()
       cy.url().should(
         'have.string',
         '/embedded/platform/dpr/request-report/report/request-examples/request-example-success/filters',
@@ -213,27 +271,67 @@ context('Requesting a report', () => {
     })
 
     it('should show the correct link in the user reports list', () => {
-      cy.get('#tab_requested-reports-tab').click()
-      cy.get(`a[href="${filtersHref}"]`).contains('Retry')
-      cy.get(`a[data-execution-id="${executionId}"]`).contains('Remove')
+      cy.findByRole('tab', { name: /Requested/ }).click()
+
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Successful Report')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Retry' })
+          cy.findByRole('link', { name: 'Remove' })
+        })
+      })
     })
 
     it('should go to filters page when clicking retry in the user catalogue', () => {
-      cy.get('#tab_requested-reports-tab').click()
-      cy.get(`a[href="${filtersHref}"`).click()
+      cy.findByRole('tab', { name: /Requested/ }).click()
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Successful Report')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Retry' }).click()
+        })
+      })
       cy.url().should('have.string', filtersHref)
     })
 
     it('should show the correct status', () => {
-      cy.get('#tab_requested-reports-tab').click()
-      cy.get(`a[data-execution-id="${executionId}"]`).parents('.govuk-table__row').contains('Successful Report')
-      cy.get(`a[data-execution-id="${executionId}"]`).parents('.govuk-table__row').contains('ABORTED')
+      cy.findByRole('tab', { name: /Requested/ }).click()
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Successful Report')
+          },
+        }).within(() => {
+          cy.findByRole('cell', { name: 'ABORTED' })
+        })
+      })
     })
 
     it('should remove the item from the list by clicking remove', () => {
-      cy.get('#tab_requested-reports-tab').click()
-      cy.get(`a[data-execution-id="${executionId}"]`).click()
-      cy.get(`a[data-execution-id="${executionId}"]`).should('not.exist')
+      cy.findByRole('tab', { name: /Requested/ }).click()
+
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Successful Report')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Remove' }).click()
+        })
+      })
+
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Successful Report')
+          },
+        }).should('not.exist')
+      })
     })
   })
 
@@ -243,8 +341,16 @@ context('Requesting a report', () => {
       'http://localhost:3010/embedded/platform/dpr/request-report/report/request-examples/request-example-expire/filters?filters.field1=value1.1&filters.field3.start=2003-02-01&filters.field3.end=2006-05-04&sortColumn=field1&sortedAsc=false'
 
     it('should setup an expired report', () => {
-      cy.get(expiredRequestUrlSelector).click()
-      cy.get('#async-request-report-button').click()
+      cy.findByLabelText(/Reports catalogue.*/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Expiring report')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Request report' }).click()
+        })
+      })
+      cy.findByRole('button', { name: 'Request report' }).click()
 
       cy.url().should(
         'match',
@@ -264,28 +370,69 @@ context('Requesting a report', () => {
 
     it('should show the correct expired links in the user reports list', () => {
       cy.visit(path)
-      cy.get('#tab_recently-viewed-tab').click()
-
+      cy.findByRole('tab', { name: /Viewed/ }).click()
       cy.reload().reload().reload()
-
-      cy.get(`a[href="${filtersHref}"`).contains('Refresh')
-      cy.get(`a[data-execution-id="${expiredExecutionId}"]`).contains('Remove')
+      cy.findByLabelText(/Viewed/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Expiring report')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Refresh' })
+          cy.findByRole('link', { name: 'Remove' })
+        })
+      })
     })
 
-    it('should go to filters page when clicking retry in the user catalogue', () => {
-      cy.get('#tab_recently-viewed-tab').click()
+    it('should go to filters page when clicking refresh in the user catalogue', () => {
+      cy.findByRole('tab', { name: /Viewed/ }).click()
+
+      cy.findByLabelText(/Viewed/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Expiring report')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Refresh' }).click()
+        })
+      })
+      cy.url().should('have.string', filtersHref)
     })
 
     it('should show the correct status', () => {
-      cy.get('#tab_recently-viewed-tab').click()
-      cy.get(`a[data-execution-id="${expiredExecutionId}"]`).parents('.govuk-table__row').contains('Expiring report')
-      cy.get(`a[data-execution-id="${expiredExecutionId}"]`).parents('.govuk-table__row').contains('EXPIRED')
+      cy.findByRole('tab', { name: /Viewed/ }).click()
+
+      cy.findByLabelText(/Viewed/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Expiring report')
+          },
+        }).within(() => {
+          cy.findByRole('cell', { name: 'EXPIRED' })
+        })
+      })
     })
 
     it('should remove the item from the list by clicking remove', () => {
-      cy.get('#tab_recently-viewed-tab').click()
-      cy.get(`a[data-execution-id="${expiredExecutionId}"]`).click()
-      cy.get(`a[data-execution-id="${expiredExecutionId}"]`).should('not.exist')
+      cy.findByRole('tab', { name: /Viewed/ }).click()
+
+      cy.findByLabelText(/Viewed/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Expiring report')
+          },
+        }).within(() => {
+          cy.findByRole('link', { name: 'Remove' }).click()
+        })
+      })
+
+      cy.findByLabelText(/Requested/i).within(() => {
+        cy.findByRole('row', {
+          name: (_, element) => {
+            return element.textContent.includes('Expiring report')
+          },
+        }).should('not.exist')
+      })
     })
   })
 })

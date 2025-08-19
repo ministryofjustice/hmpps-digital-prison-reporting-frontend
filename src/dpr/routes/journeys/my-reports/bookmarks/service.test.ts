3d3ba86eb0fe62fc64214946906d5565398dfaa7
rossdowthwaite <rossdowthwaite@gmail.com>
2025-08-19 11:26:11 +0100

    bookmarking tests

--- src/dpr/routes/journeys/my-reports/bookmarks/service.test.ts
+++ src/dpr/routes/journeys/my-reports/bookmarks/service.test.ts
@@ -180,8 +180,8 @@ describe('BookmarkService', () => {
       })
 
       expect(res)
-        .toEqual(`<div class='dpr-bookmark dpr-bookmark-table' data-dpr-module='bookmark-toggle' aria-labelledby='variant-id-report-id-context-id-bookmark-label'>
-  <input class='bookmark-input' aria-label='bookmark toggle' type='checkbox' id='report-id-variant-id-context-id' data-report-id='report-id' data-id='variant-id' data-report-type='report' data-csrf-token='csrfToken' null />
+        .toEqual(`<div class='dpr-bookmark dpr-bookmark-table' data-dpr-module='bookmark-toggle' aria-label='bookmark toggle'>
+  <input class='bookmark-input' type='checkbox' id='report-id-variant-id-context-id' data-report-id='report-id' data-id='variant-id' data-report-type='report' data-csrf-token='csrfToken' null />
   <label tabindex='0' id='variant-id-report-id-context-id-bookmark-label' for='report-id-variant-id-context-id'><span class='dpr-bookmark-label govuk-body-s'>Add bookmark</span></label>
 </div>`)
     })

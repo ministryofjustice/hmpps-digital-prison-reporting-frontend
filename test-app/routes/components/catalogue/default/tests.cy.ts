28e97fdd92fcf85e13f614e5b3a53453fc99963c
James Reed <james.reed1@justice.gov.uk>
2025-10-01 11:28:51 +0100

    fix test

--- test-app/routes/components/catalogue/default/tests.cy.ts
+++ test-app/routes/components/catalogue/default/tests.cy.ts
@@ -138,10 +138,10 @@ context('Catalogue component', () => {
           cy.findAllByRole('row').each((row) => {
             cy.wrap(row).within(() => {
               cy.findAllByRole('cell').each((cell, index) => {
-                cy.wrap(cell).within((cell) => {
+                cy.wrap(cell).within(() => {
                   if (index === 3) {
-                    const requestLink = cy.findByRole('link', { name: /Request / })
-                    const isAlreadyBookmarkedRow = requestLink.invoke('attr', 'href').then((href) => {
+                    const requestLink = cy.findByRole('link', { name: /(Request|Load) (Report|Dashboard)/i })
+                    requestLink.invoke('attr', 'href').then((href) => {
                       if (!automaticBookmarkConfig.caseloads.KMI.some(caseload => href?.includes(caseload.variantId))) {
                         cy.findByLabelText(/Add bookmark|Remove bookmark/g).should('exist')
                       }

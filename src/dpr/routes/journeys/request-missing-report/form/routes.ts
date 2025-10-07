e2c252e40906cf27260ead4fc3229d9fde632345
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2025-10-07 15:50:18 +0100

    fix couple more issues (#512)
    Co-authored-by: James Reed <james.reed1@justice.gov.uk>

--- src/dpr/routes/journeys/request-missing-report/form/routes.ts
+++ src/dpr/routes/journeys/request-missing-report/form/routes.ts
@@ -1,9 +1,9 @@
 /* eslint-disable no-param-reassign */
 import { Router } from 'express'
-import { validate } from 'src/dpr/validation/validate'
 import MissingReportFormController from './controller'
 import { Services } from '../../../../types/Services'
 import { schema } from './validation'
+import { validate } from '../../../../validation/validate'
 
 export default function routes({ layoutPath, services }: { layoutPath: string; services: Services }) {
   const router = Router({ mergeParams: true })

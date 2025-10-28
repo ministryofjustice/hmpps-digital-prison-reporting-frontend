1ecea517c5c00193593d7dde3c3847a7f7c80916
rossdowthwaite <rossdowthwaite@gmail.com>
2025-10-28 14:26:54 +0000

    cypress test

--- src/dpr/routes/journeys/view-report/async/report/tests.cy.ts
+++ src/dpr/routes/journeys/view-report/async/report/tests.cy.ts
@@ -1102,7 +1102,6 @@ context('Viewing a report', () => {
         cy.findByRole('link', { name: 'Field 2' }).should('have.class', 'data-table-header-button-sort-none')
         cy.location().should((location) => {
           expect(location.search).to.contain(`&sortColumn=field1&sortedAsc=false`)
-          expect(location.search).to.contain(`filters.field7=value8.2&filters.field7=value8.3`)
         })
 
         // Sort asc by field 1
@@ -1111,7 +1110,6 @@ context('Viewing a report', () => {
         cy.findByRole('link', { name: 'Field 2' }).should('have.class', 'data-table-header-button-sort-none')
         cy.location().should((location) => {
           expect(location.search).to.contain(`&sortColumn=field1&sortedAsc=true`)
-          expect(location.search).to.contain(`filters.field7=value8.2&filters.field7=value8.3`)
         })
 
         // Sort asc by field 2
@@ -1120,7 +1118,6 @@ context('Viewing a report', () => {
         cy.findByRole('link', { name: 'Field 1' }).should('have.class', 'data-table-header-button-sort-none')
         cy.location().should((location) => {
           expect(location.search).to.contain(`&sortColumn=field2&sortedAsc=true`)
-          expect(location.search).to.contain(`filters.field7=value8.2&filters.field7=value8.3`)
         })
 
         // Sort desc by field 2
@@ -1129,7 +1126,6 @@ context('Viewing a report', () => {
         cy.findByRole('link', { name: 'Field 1' }).should('have.class', 'data-table-header-button-sort-none')
         cy.location().should((location) => {
           expect(location.search).to.contain(`&sortColumn=field2&sortedAsc=false`)
-          expect(location.search).to.contain(`filters.field7=value8.2&filters.field7=value8.3`)
         })
       })
     })

d83c2353b110d4d6812bca1e7de2167591886147
rossdowthwaite <rossdowthwaite@gmail.com>
2025-11-10 12:46:25 +0000

    Remove logs from tests

--- src/dpr/utils/logger.ts
+++ src/dpr/utils/logger.ts
@@ -4,6 +4,7 @@ import bunyanFormat from 'bunyan-format'
 const formatOut = bunyanFormat({ outputMode: 'short', color: false })
 
 const disableLogs = process.env.SUPPRESS_LOGS
+console.log({ disableLogs })
 export const logger = bunyan.createLogger({
   name: 'Digital Prison Reporting:',
   stream: formatOut,

57d37172f96150617a3e30cb8dee9deaf3b84112
rossdowthwaite <rossdowthwaite@gmail.com>
2025-12-03 09:19:14 +0000

    PR linting

--- _networkMocks/dashboard/dashboardDefinitions/visualisations/index.ts
+++ _networkMocks/dashboard/dashboardDefinitions/visualisations/index.ts
@@ -2,12 +2,10 @@ import lists from './lists'
 import matrix from './matrix'
 import charts from './charts'
 import scorecards from './scorecards'
-import scorecardGroups from './scorecard-groups'
 
 export default {
   lists,
   charts,
   matrix,
   scorecards,
-  scorecardGroups,
 }

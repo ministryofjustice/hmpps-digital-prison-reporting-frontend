0b652ee2499a725db788374e7fa206037366a126
rossdowthwaite <rossdowthwaite@gmail.com>
2025-12-03 13:31:12 +0000

    PR linting

--- test-app/mocks/mockClients/dashboards/definitions/test-dashboard/index.js
+++ test-app/mocks/mockClients/dashboards/definitions/test-dashboard/index.js
@@ -1,6 +1,5 @@
 // @ts-nocheck
 const def1 = require('./dashboard-definiton-1')
 const def2 = require('./dashboard-definition-2')
-const barcharttest = require('./bar-chart-test')
 
-module.exports = [def1, def2, barcharttest]
+module.exports = [def1, def2]

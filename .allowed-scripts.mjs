89b9947f02630a3c4e8c1a9686b936edcb328f81
James Reed <james.reed1@justice.gov.uk>
2026-01-06 10:04:44 +0000

    update esbuild, remove unneeded libs and config

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -3,11 +3,10 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 export default configureAllowedScripts({
   allowlist: {
     'node_modules/cypress@14.5.4': 'ALLOW',
-    'node_modules/esbuild@0.25.5': 'ALLOW',
+    'node_modules/esbuild@0.27.2': 'ALLOW',
     'node_modules/@parcel/watcher@2.5.1': 'ALLOW',
     'node_modules/dtrace-provider@0.8.8': 'FORBID',
     'node_modules/fsevents@2.3.3': 'FORBID',
-    'node_modules/unrs-resolver@1.9.2': 'FORBID',
-    'node_modules/@badeball/cypress-cucumber-preprocessor@22.2.0': 'FORBID',
+    'node_modules/unrs-resolver@1.9.2': 'FORBID'
   },
 })

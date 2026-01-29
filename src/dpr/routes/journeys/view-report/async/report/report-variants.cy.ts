4496147dfc63e9c633901c0926dc9af8eddd06d9
rossdowthwaite <rossdowthwaite@gmail.com>
2026-01-29 13:57:37 +0000

    int tests

--- src/dpr/routes/journeys/view-report/async/report/report-variants.cy.ts
+++ src/dpr/routes/journeys/view-report/async/report/report-variants.cy.ts
@@ -30,6 +30,8 @@ context('Viewing a report', () => {
         requestReportByNameAndDescription(listSectionReport)
         cy.findByRole('heading', { level: 1, name: /List-section/ })
         cy.findAllByRole('heading', { name: /First.*Second/ }).should('have.length', 4)
+
+        // Test headings exist
         cy.findByRole('heading', { name: /First: One, Second: A 4 results/ })
           .should('be.visible')
           .scrollIntoView()
@@ -42,6 +44,28 @@ context('Viewing a report', () => {
         cy.findByRole('heading', { name: /First: Two, Second: B 7 results/ })
           .should('be.visible')
           .scrollIntoView()
+
+        // Test they only include 1 table per section
+        cy.findAllByLabelText(/First: One, Second: A 4 results/)
+          .eq(1)
+          .within(() => {
+            cy.findAllByRole('table').should('have.length', 1)
+          })
+        cy.findAllByLabelText(/First: One, Second: B 6 results/)
+          .eq(1)
+          .within(() => {
+            cy.findAllByRole('table').should('have.length', 1)
+          })
+        cy.findAllByLabelText(/First: Two, Second: A 3 results/)
+          .eq(1)
+          .within(() => {
+            cy.findAllByRole('table').should('have.length', 1)
+          })
+        cy.findAllByLabelText(/First: Two, Second: B 7 results/)
+          .eq(1)
+          .within(() => {
+            cy.findAllByRole('table').should('have.length', 1)
+          })
       })
 
       it('should sort the sections correctly', () => {
@@ -76,19 +100,19 @@ context('Viewing a report', () => {
       })
     })
 
-    // describe('summary-section', () => {
-    //   it('should display a summary section variant', () => {
-    //     cy.task('stubSummarySectionDefinitionRequest')
-    //     cy.task('stubAsyncSummaryReport')
-    //     cy.task('stubResultSuccessResultDifferentValues')
+    describe('summary-section', () => {
+      it('should display a summary section variant', () => {
+        cy.task('stubSummarySectionDefinitionRequest')
+        cy.task('stubAsyncSummaryReport')
+        cy.task('stubResultSuccessResultDifferentValues')
 
-    //     cy.visit(path)
-    //     requestReportByNameAndDescription({ name: 'Summary-section', description: 'Summary-section template' })
-    //     cy.findByRole('heading', { level: 1, name: /Summary-section/ }).should('be.visible')
+        cy.visit(path)
+        requestReportByNameAndDescription({ name: 'Summary-section', description: 'Summary-section template' })
+        cy.findByRole('heading', { level: 1, name: /Summary-section/ }).should('be.visible')
 
-    //     // TODO: this test
-    //   })
-    // })
+        // TODO: this test
+      })
+    })
 
     describe('parent-child-section', () => {
       const ParentChildSectionReport = {
@@ -106,7 +130,6 @@ context('Viewing a report', () => {
         requestReportByNameAndDescription(ParentChildSectionReport)
         cy.findByRole('heading', { level: 1, name: /Parent-child-section/ }).should('be.visible')
 
-        // First child table
         cy.findByRole('heading', { name: /Section 1: One, Section 2: A 5 results/ })
           .scrollIntoView()
           .should('be.visible')
@@ -120,7 +143,65 @@ context('Viewing a report', () => {
           .scrollIntoView()
           .should('be.visible')
 
-        // TODO: more tests
+        cy.findAllByLabelText('Section 1: One, Section 2: A 5 results')
+          .eq(1)
+          .within(() => {
+            cy.findAllByRole('table').should('have.length', 10)
+
+            cy.findAllByLabelText('Parent 1')
+              .eq(0)
+              .within(() => {
+                cy.findAllByRole('table').should('have.length', 2)
+
+                // parent
+                cy.findAllByLabelText('Parent 1')
+                  .eq(0)
+                  .within(() => {
+                    cy.findByRole('table').within(() => {
+                      cy.findAllByRole('row').should('have.length', 4)
+                    })
+                  })
+
+                // Child
+                cy.findAllByLabelText('Child Report')
+                  .eq(0)
+                  .within(() => {
+                    cy.findByRole('table').within(() => {
+                      cy.findAllByRole('row').should('have.length', 23)
+                    })
+                  })
+              })
+          })
+
+        cy.findAllByLabelText('Section 1: Two, Section 2: A 8 results')
+          .eq(1)
+          .within(() => {
+            cy.findAllByRole('table').should('have.length', 16)
+
+            cy.findAllByLabelText('Parent 1')
+              .eq(0)
+              .within(() => {
+                cy.findAllByRole('table').should('have.length', 2)
+
+                // parent
+                cy.findAllByLabelText('Parent 1')
+                  .eq(0)
+                  .within(() => {
+                    cy.findByRole('table').within(() => {
+                      cy.findAllByRole('row').should('have.length', 4)
+                    })
+                  })
+
+                // Child
+                cy.findAllByLabelText('Child Report')
+                  .eq(0)
+                  .within(() => {
+                    cy.findByRole('table').within(() => {
+                      cy.findAllByRole('row').should('have.length', 23)
+                    })
+                  })
+              })
+          })
       })
 
       it('should sort the sections correctly', () => {
@@ -153,8 +234,6 @@ context('Viewing a report', () => {
                 break
             }
           })
-
-        // TODO: more tests
       })
     })
 
@@ -167,7 +246,73 @@ context('Viewing a report', () => {
         requestReportByNameAndDescription({ name: 'Parent-child', description: 'Parent-child template' })
         cy.findByRole('heading', { level: 1, name: /Parent-child/ }).should('be.visible')
 
-        // TODO: this test
+        // Parent child group
+        cy.findAllByLabelText('Parent 1')
+          .eq(0)
+          .within(() => {
+            cy.findAllByRole('table').should('have.length', 3)
+
+            // parent
+            cy.findAllByLabelText('Parent 1')
+              .eq(0)
+              .within(() => {
+                cy.findByRole('table').within(() => {
+                  cy.findAllByRole('row').should('have.length', 4)
+                })
+              })
+
+            // child
+            cy.findAllByLabelText('Child Report')
+              .eq(0)
+              .within(() => {
+                cy.findByRole('table').within(() => {
+                  cy.findAllByRole('row').should('have.length', 10)
+                })
+              })
+
+            // child 2
+            cy.findAllByLabelText('Child Report 2')
+              .eq(0)
+              .within(() => {
+                cy.findByRole('table').within(() => {
+                  cy.findAllByRole('row').should('have.length', 10)
+                })
+              })
+          })
+
+        // Parent child group
+        cy.findAllByLabelText('Parent 8')
+          .eq(0)
+          .within(() => {
+            cy.findAllByRole('table').should('have.length', 3)
+
+            // parent
+            cy.findAllByLabelText('Parent 8')
+              .eq(0)
+              .within(() => {
+                cy.findByRole('table').within(() => {
+                  cy.findAllByRole('row').should('have.length', 4)
+                })
+              })
+
+            // child
+            cy.findAllByLabelText('Child Report')
+              .eq(0)
+              .within(() => {
+                cy.findByRole('table').within(() => {
+                  cy.findAllByRole('row').should('have.length', 16)
+                })
+              })
+
+            // child 2
+            cy.findAllByLabelText('Child Report 2')
+              .eq(0)
+              .within(() => {
+                cy.findByRole('table').within(() => {
+                  cy.findAllByRole('row').should('have.length', 16)
+                })
+              })
+          })
       })
     })
   })

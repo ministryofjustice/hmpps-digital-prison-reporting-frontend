a1e256eb578824e51d83a01f649af21a69640d0b
rossdowthwaite <rossdowthwaite@gmail.com>
2026-01-29 14:16:56 +0000

    int tests for report variants

--- src/dpr/routes/journeys/view-report/async/report/report-variants.cy.ts
+++ src/dpr/routes/journeys/view-report/async/report/report-variants.cy.ts
@@ -110,7 +110,53 @@ context('Viewing a report', () => {
         requestReportByNameAndDescription({ name: 'Summary-section', description: 'Summary-section template' })
         cy.findByRole('heading', { level: 1, name: /Summary-section/ }).should('be.visible')
 
-        // TODO: this test
+        cy.findAllByLabelText(/First: One, Second: A/)
+          .eq(0)
+          .within(() => {
+            cy.findAllByRole('table')
+              .should('have.length', 2)
+              .each((table) => {
+                cy.wrap(table).within(() => {
+                  cy.findAllByRole('row').should('have.length', 5)
+                })
+              })
+          })
+
+        cy.findAllByLabelText(/First: One, Second: B/)
+          .eq(0)
+          .within(() => {
+            cy.findAllByRole('table')
+              .should('have.length', 2)
+              .each((table) => {
+                cy.wrap(table).within(() => {
+                  cy.findAllByRole('row').should('have.length', 7)
+                })
+              })
+          })
+
+        cy.findAllByLabelText(/First: Two, Second: A/)
+          .eq(0)
+          .within(() => {
+            cy.findAllByRole('table')
+              .should('have.length', 2)
+              .each((table) => {
+                cy.wrap(table).within(() => {
+                  cy.findAllByRole('row').should('have.length', 4)
+                })
+              })
+          })
+
+        cy.findAllByLabelText(/First: Two, Second: B/)
+          .eq(0)
+          .within(() => {
+            cy.findAllByRole('table')
+              .should('have.length', 2)
+              .each((table) => {
+                cy.wrap(table).within(() => {
+                  cy.findAllByRole('row').should('have.length', 8)
+                })
+              })
+          })
       })
     })
 

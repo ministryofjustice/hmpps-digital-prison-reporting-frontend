6b58a226d912846abd9f76a371df6de27ebf6620
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-02-09 20:22:42 +0000

    All flag results are expected to booleans

--- src/dpr/types/express.d.ts
+++ src/dpr/types/express.d.ts
@@ -1,13 +1,9 @@
-import { Flag } from '@flipt-io/flipt'
-
-declare global {
-  namespace Express {
-    interface Locals {
-      featureFlags: {
-        lastUpdated: number
-        flags: {
-          [flagName: string]: Flag
-        }
+declare namespace Express {
+  interface Locals {
+    featureFlags: {
+      lastUpdated: number
+      flags: {
+        [flagName: string]: boolean
       }
     }
   }

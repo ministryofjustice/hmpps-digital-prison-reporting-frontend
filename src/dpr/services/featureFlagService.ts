5436ccae9678cd6848763f5fee8a0c09c2b5b0a7
thomas.geraghty <32871802+Thomas-Geraghty@users.noreply.github.com>
2026-02-09 20:50:36 +0000

    Remove use of the listFlags management endpoint, utilise `batch` eval instead

--- src/dpr/services/featureFlagService.ts
+++ src/dpr/services/featureFlagService.ts
@@ -1,13 +1,12 @@
-import { FliptClient, ListFlagsResponse } from '@flipt-io/flipt'
+import { FliptClient } from '@flipt-io/flipt'
 import { Application } from 'express'
 import { FeatureFlagConfig } from '../data/types'
-
-// Override this until types are fixed - metadata is returned, but not in the types
-declare module '@flipt-io/flipt' {
-  interface Flag {
-    metadata: Record<string, string | boolean | number>
-  }
-}
+import {
+  FEATURE_FLAG_NAMESPACE,
+  getFeatureFlagFallbackState,
+  type FeatureFlagEvaluationSubject,
+  type FeatureFlagKey,
+} from '../utils/featureFlagsHelper'
 
 export class FeatureFlagService {
   restClient: FliptClient | undefined
@@ -26,34 +25,65 @@ export class FeatureFlagService {
       },
       url,
     })
-    this.namespace = 'hmpps-digital-prison-reporting'
+    this.namespace = FEATURE_FLAG_NAMESPACE
   }
 
-  async getFlags(): Promise<ListFlagsResponse> {
-    if (!this.restClient || !this.namespace) {
-      return {
-        flags: [],
-        nextPageToken: '',
-        totalCount: 0,
-      }
+  async evaluateBooleanFlag(flagKey: FeatureFlagKey, subject: FeatureFlagEvaluationSubject): Promise<boolean> {
+    const evaluation = await this.evaluateBooleanFlags([flagKey], subject)
+    return evaluation[flagKey]
+  }
+
+  async evaluateBooleanFlags<TFlag extends FeatureFlagKey>(
+    flagKeys: readonly TFlag[],
+    subject: FeatureFlagEvaluationSubject,
+  ): Promise<Record<TFlag, boolean>> {
+    const results = Object.fromEntries(
+      flagKeys.map((flagKey) => [flagKey, getFeatureFlagFallbackState(flagKey)]),
+    ) as Record<TFlag, boolean>
+
+    if (!this.restClient || !this.namespace || flagKeys.length === 0) {
+      return results
     }
-    return this.restClient.flags.listFlags(this.namespace)
+
+    const namespaceKey = this.namespace
+
+    try {
+      const batchResponse = await this.restClient.evaluation.batch({
+        requests: flagKeys.map((flagKey) => ({
+          namespaceKey,
+          flagKey,
+          entityId: subject.entityId,
+          context: subject.context,
+        })),
+      })
+
+      batchResponse.responses
+        .filter((response) => response.type === 'BOOLEAN_EVALUATION_RESPONSE_TYPE')
+        .forEach((response) => {
+          const flagKey = response.booleanResponse?.flagKey as TFlag | undefined
+          const enabled = response.booleanResponse?.enabled
+
+          if (flagKey && enabled !== undefined && flagKey in results) {
+            results[flagKey] = enabled
+          }
+        })
+    } catch (_error) {
+      return results
+    }
+
+    return results
   }
 }
 
 const resolveFlag = (app: Application, flagName: string) => {
-  const flag = app.locals.featureFlags?.flags?.[flagName]
-  if (flag && flag.type !== 'BOOLEAN_FLAG_TYPE') {
-    throw Error('Tried to validate whether a non-boolean flag was enabled')
-  }
-  return flag
+  return app.locals.featureFlags?.flags?.[flagName]
 }
 
 export const isBooleanFlagEnabledOrMissing = (flagName: string, app: Application): boolean => {
   const flag = resolveFlag(app, flagName)
-  return !flag || flag.enabled
+  return flag !== false
 }
 
 export const isBooleanFlagExplicitlyEnabled = (flagName: string, app: Application): boolean => {
-  return resolveFlag(app, flagName)?.enabled === true
+  return resolveFlag(app, flagName) === true
 }

a4f7c08db0aac53f7f3df78cb29b86a630ecf698
rossdowthwaite <rossdowthwaite@gmail.com>
2026-02-18 10:34:46 +0000

    Accessibility label. Update partial dataset mock

--- src/dpr/components/_charts/chart/Chart.ts
+++ src/dpr/components/_charts/chart/Chart.ts
@@ -44,6 +44,9 @@ class Chart {
       const label = this.createDatasetLabel(row)
       const data = this.createDatasetValues(measures, row)
       const total = data.reduce((acc: number, val: number) => acc + val, 0)
+      const things = {
+        
+      }
 
       return {
         label,

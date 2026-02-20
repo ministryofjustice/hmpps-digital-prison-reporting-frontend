404e20bd23e012f4e0ec85f1ee434d4d8021bdc3
James Reed <james.reed1@justice.gov.uk>
2026-02-20 11:42:21 +0000

    start for making empty data dashboard tests

---
+++ _networkMocks/dashboard/data/empty-data/data.ts
@@ -0,0 +1,10 @@
+import { completeDataSet } from "../complete-data/data"
+
+export const undefinedDashboardData = undefined
+
+export const emptyDashboardData = []
+
+export const missingFirstRowDashboardData = [
+  undefined,
+  ...completeDataSet.slice(1)
+]

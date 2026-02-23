3b89b95b0efd316c9801e5fae6506d52abb52bdb
Gavriil <135604838+gavriil-g-moj@users.noreply.github.com>
2026-02-23 15:59:29 +0000

    Dpr2 2603 enable new sync download (#715)
    Updated the FE to use the new streaming download also for the sync
    reports.

--- cypress-tests/mockApis/reporting.ts
+++ cypress-tests/mockApis/reporting.ts
@@ -14,6 +14,7 @@ import {
   getAsyncInteractiveCountMock,
   getAsyncReportResultMock,
   getAsyncReportDownloadMock,
+  getSyncReportDownloadMock,
   getReportResultCountMock,
   reportsAbortedStatusMock,
   reportsExpiredStatusMock,
@@ -631,6 +632,7 @@ export const stubs = {
   stubMissingRequestSubmitSuccess: () => stubFor(missingReportSubmitSuccessMock),
   stubMissingRequestSubmitFail: () => stubFor(missingReportSubmitFailMock),
   stubAsyncReportDownload: () => stubFor(getAsyncReportDownloadMock),
+  stubSyncReportDownload: () => stubFor(getSyncReportDownloadMock),
   stubDefinitionUnprintable: () =>
     stubFor(setupSimpleReportDefinitionResponseMock(`feature-testing`, featureTestingUnprintable)),
   stubDefinitionEmptyReport: () =>

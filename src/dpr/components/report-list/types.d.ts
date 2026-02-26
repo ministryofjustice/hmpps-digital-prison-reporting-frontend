27ec86f737da5a83650af4e36ea652284ad8641d
James Reed <james.reed1@justice.gov.uk>
2026-02-26 12:02:53 +0000

    remove all of the report-list old sync code

--- src/dpr/components/report-list/types.d.ts
+++
@@ -1,6 +0,0 @@
-import { ListWithWarnings } from '../../data/types'
-
-export interface ListDataSources {
-  data: Promise<Record<string, string>[]> | Promise<ListWithWarnings>
-  count: Promise<number>
-}

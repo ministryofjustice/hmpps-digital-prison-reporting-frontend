85834432c884d5c34cb4f3ba743f6f4079674d42
rossdowthwaite <rossdowthwaite@gmail.com>
2026-02-26 09:10:53 +0000

    Update snapshots tests

---
+++ src/dpr/components/_dashboards/visual-regression-tests/mixed-charts-partial-historic-dataset.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test'
+
+test('Mixed chart partial historic dataset', async ({ page }) => {
+  await page.goto('http://localhost:3010/embedded/platform/')
+
+  page.getByLabel(/Reports catalogue.*/i)
+  await page
+    .getByLabel(/Reports catalogue.*/i)
+    .locator(page.getByRole('row').filter({ hasText: /Mixed - Historic - Partial dataset/ }))
+    .locator(page.getByRole('link', { name: /Request dashboard/ }))
+    .click()
+
+  await page.getByRole('button', { name: /Request dashboard/ }).click()
+
+  await expect(page.getByRole('heading', { name: /Mixed - Historic - Partial dataset/ })).toBeVisible()
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    animations: 'disabled',
+    maxDiffPixelRatio: 0.01,
+  })
+})

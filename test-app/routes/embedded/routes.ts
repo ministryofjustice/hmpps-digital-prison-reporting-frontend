33f3bcfe8d8eb6639ae030f30f64578b69fae1ef
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2026-02-26 15:16:19 +0000

    remove deprecated and now unused sync non-route report list code (#724)
    Co-authored-by: James Reed <james.reed1@justice.gov.uk>

--- test-app/routes/embedded/routes.ts
+++ test-app/routes/embedded/routes.ts
@@ -4,7 +4,6 @@ import setUpNestedRoute from '../../../src/dpr/middleware/setUpNestedRoute'
 import EmbeddedController from './controller'
 
 // Routes
-import SyncRoutes from './sync-report/routes'
 import PlatformRoutes from './platform/routes'
 import { Services } from '../../../src/dpr/types/Services'
 
@@ -15,7 +14,6 @@ export function Routes(services: Services): Router {
   router.get('/', controller.GET)
 
   router.use(`/platform/`, setUpNestedRoute(), PlatformRoutes(services))
-  router.use(`/sync`, SyncRoutes())
 
   return router
 }

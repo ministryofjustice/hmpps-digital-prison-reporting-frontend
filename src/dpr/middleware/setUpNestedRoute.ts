efeb26ae2323fc4e2c68d92f86e1065f00ddff01
rossdowthwaite <rossdowthwaite@gmail.com>
2026-03-26 16:28:11 +0000

    Updated nested middleware

--- src/dpr/middleware/setUpNestedRoute.ts
+++ src/dpr/middleware/setUpNestedRoute.ts
@@ -9,31 +9,27 @@ export const setupNestedRoute = (): RequestHandler => {
     const locals = res.app.locals.dprPaths
     const base = req.baseUrl && req.baseUrl.length > 0 ? req.baseUrl : undefined
 
-    // If no base or paths uninitialized: nothing to do
-    if (!base) {
-      return next()
+    if (base) {
+      const currentBase = locals.nestedBaseUrl
+
+      if (currentBase !== base) {
+        locals.nestedBaseUrl = base
+
+        locals.bookmarkActionEndpoint = setNestedPath(locals.bookmarkActionEndpoint, base)
+        locals.downloadActionEndpoint = setNestedPath(locals.downloadActionEndpoint, base)
+        locals.productCollectionEndpoint = setNestedPath(locals.productCollectionEndpoint, base)
+        locals.bookmarkListPath = setNestedPath(locals.bookmarkListPath, base)
+        locals.requestedListPath = setNestedPath(locals.requestedListPath, base)
+        locals.recentlyViewedListPath = setNestedPath(locals.recentlyViewedListPath, base)
+        locals.reportsCatalogue = setNestedPath(locals.reportsCatalogue, base)
+        locals.userReportsList = setNestedPath(locals.userReportsList, base)
+        locals.dprHomepage = setNestedPath(locals.dprHomepage, base)
+
+        logger.info('Paths updated with nested base url:', { updatedpaths: locals })
+      }
     }
 
-    const currentBase = locals.nestedBaseUrl
-
-    // Only update paths if base is NEW or CHANGED
-    if (currentBase !== base) {
-      locals.nestedBaseUrl = base
-
-      locals.bookmarkActionEndpoint = setNestedPath(locals.bookmarkActionEndpoint, base)
-      locals.downloadActionEndpoint = setNestedPath(locals.downloadActionEndpoint, base)
-      locals.productCollectionEndpoint = setNestedPath(locals.productCollectionEndpoint, base)
-      locals.bookmarkListPath = setNestedPath(locals.bookmarkListPath, base)
-      locals.requestedListPath = setNestedPath(locals.requestedListPath, base)
-      locals.recentlyViewedListPath = setNestedPath(locals.recentlyViewedListPath, base)
-      locals.reportsCatalogue = setNestedPath(locals.reportsCatalogue, base)
-      locals.userReportsList = setNestedPath(locals.userReportsList, base)
-      locals.dprHomepage = setNestedPath(locals.dprHomepage, base)
-
-      logger.info('Paths updated with nested base url:', { updatedpaths: locals })
-    }
-
-    next()
+    return next()
   }
 }
 

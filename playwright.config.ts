c5d5281a207dd9e2c5728c1e965d2c3d30f47194
rossdowthwaite <rossdowthwaite@gmail.com>
2026-04-20 12:52:25 +0100

    deviceScaleFactor

--- playwright.config.ts
+++ playwright.config.ts
@@ -42,7 +42,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices['Desktop Chrome'], deviceScaleFactor: 1 },
     },
 
     {

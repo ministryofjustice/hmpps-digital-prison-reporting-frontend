92d140005c8a7107a04752d7f1951afa15ac61f5
Ross Dowthwaite <rossdowthwaite@gmail.com>
2026-05-01 16:44:45 +0100

    Dpr2 autocomplete bug 5 (#883)
    - Standardised button link styling
    - More Debug logs for investigating issue with personalisation
    - Styling fix - padding on date filters

--- src/dpr/services/featureFlagService.ts
+++ src/dpr/services/featureFlagService.ts
@@ -66,7 +66,7 @@ export class FeatureFlagService {
       flagKeys.map((flagKey) => [flagKey, getFeatureFlagFallbackState(flagKey)]),
     ) as Record<TFlag, boolean>
 
-    logger.info(`PERSONALISATION DEBUG: evaluateBooleanFlags: ${results}`)
+    logger.info(`PERSONALISATION DEBUG: evaluateBooleanFlags: ${JSON.stringify(results)}`)
 
     if (flagKeys.length === 0) {
       return results
@@ -114,6 +114,6 @@ const resolveFlag = (app: Application, flagName: string): boolean | undefined =>
 
 export const isBooleanFlagEnabledOrMissing = (flagName: string, app: Application): boolean => {
   const flag = resolveFlag(app, flagName)
-  logger.info(`PERSONALISATION DEBUG: isBooleanFlagEnabledOrMissing: ${flagName} : ${flag}`)
+  logger.info(`PERSONALISATION DEBUG: isBooleanFlagEnabledOrMissing: ${flagName} : ${JSON.stringify(flag)}`)
   return flag !== false
 }

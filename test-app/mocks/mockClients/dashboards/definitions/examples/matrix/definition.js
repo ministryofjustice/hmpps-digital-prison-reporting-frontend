e684ae2578a1ee1a381c6edfe81e97c52476b962
Sunil Khutan <sunil.khutan@justice.gov.uk>
2026-05-12 13:26:07 +0100

    re-enable matrix definitions

--- test-app/mocks/mockClients/dashboards/definitions/examples/matrix/definition.js
+++ test-app/mocks/mockClients/dashboards/definitions/examples/matrix/definition.js
@@ -14,28 +14,28 @@ const dataQualityHistoric = {
         'Examples of heatmaps charts where buckets are defined automatically using the values in the data, to produce 3 buckets of equal size',
       visualisations: [
         matrix.automaticBucketing,
-        // matrix.automaticBucketingCustomBaseColour,
-        // matrix.automaticBucketingRag,
-        // matrix.automaticBucketingCustomColours,
+        matrix.automaticBucketingCustomBaseColour,
+        matrix.automaticBucketingRag,
+        matrix.automaticBucketingCustomColours,
       ],
     },
-    // {
-    //   id: 'matrix-test-rag',
-    //   display: 'User defined custom buckets',
-    //   description:
-    //     'Examples of heatmaps where the bucket count, sizing and colourings are defined in the visualisation definition',
-    //   visualisations: [
-    //     matrix.customBucketsWithSizing,
-    //     matrix.customBucketsWithSizingOpen,
-    //     matrix.customBucketsWithSizingAndColour,
-    //   ],
-    // },
-    // {
-    //   id: 'matrix-test-alldata',
-    //   display: 'Matrix all data',
-    //   description: '',
-    //   visualisations: [lists.fullDatasetOverTime],
-    // },
+    {
+      id: 'matrix-test-rag',
+      display: 'User defined custom buckets',
+      description:
+        'Examples of heatmaps where the bucket count, sizing and colourings are defined in the visualisation definition',
+      visualisations: [
+        matrix.customBucketsWithSizing,
+        matrix.customBucketsWithSizingOpen,
+        matrix.customBucketsWithSizingAndColour,
+      ],
+    },
+    {
+      id: 'matrix-test-alldata',
+      display: 'Matrix all data',
+      description: '',
+      visualisations: [lists.fullDatasetOverTime],
+    },
   ],
   filterFields: [granularDateRangeFilter],
 }

d4e78db2cd61e16ac55d6c01bbd8b32c3de98ddc
Sunil <skhutan@users.noreply.github.com>
2026-05-19 10:03:19 +0100

    DPR 2844 fix ts-nocheck (#936)

--- src/dpr/components/show-more/clientClass.ts
+++ src/dpr/components/show-more/clientClass.ts
@@ -1,37 +1,50 @@
-// @ts-nocheck
 import { DprClientClass } from '../../DprClientClass'
 
 class ShowMore extends DprClientClass {
-  static getModuleName() {
+  static override getModuleName() {
     return 'show-more'
   }
 
-  initialise() {
+  override initialise() {
     this.initShowMore()
   }
 
   initShowMore() {
     const element = this.getElement()
     const textContainer = element.querySelector('.dpr-show-more-content')
-    const button = element.querySelector('.dpr-show-hide-button')
-    const textContent = element.getAttribute('data-content')
-    const length = +element.getAttribute('data-length') || 200
+    const button = element.querySelector<HTMLElement>('.dpr-show-hide-button')
+    const textContent = element.getAttribute('data-content') ?? ''
+    const length = Number(element.getAttribute('data-length')) || 200
     const shortString = textContent.replaceAll(/<[^>]+>/g, '').substring(0, length)
 
     if (textContent.length > length) {
-      textContainer.innerHTML = `${shortString}... `
+      if (textContainer) {
+        textContainer.innerHTML = `${shortString}... `
+      }
     } else {
-      textContainer.innerHTML = `${textContent}`
-      button.style.display = 'none'
+      if (textContainer) {
+        textContainer.innerHTML = `${textContent}`
+      }
+
+      if (button) {
+        button.style.display = 'none'
+      }
+      
     }
 
-    button.addEventListener('click', (event) => {
+    button?.addEventListener('click', (event) => {
       event.preventDefault()
       if (button.innerHTML === 'show more') {
-        textContainer.innerHTML = `${textContent}  `
+        if(textContainer) {
+          textContainer.innerHTML = `${textContent}  `
+        }
+        
         button.innerHTML = 'show less'
       } else if (button.innerHTML === 'show less') {
-        textContainer.innerHTML = `${shortString}... `
+          if (textContainer) {
+            textContainer.innerHTML = `${shortString}... `
+          }
+       
         button.innerHTML = 'show more'
       }
     })

38150a428079b359c33ed3824aec8ed9d51aded3
rossdowthwaite <rossdowthwaite@gmail.com>
2026-05-21 15:14:54 +0100

    linting

--- _networkMocks/report/mockVariants/filter-input-examples/index.ts
+++ _networkMocks/report/mockVariants/filter-input-examples/index.ts
@@ -4,4 +4,10 @@ import { establishmentAutocomplete } from './autocomplete'
 import multiselectExample from './multiselect'
 import autoCompleteMultiExample from './autocompleteMulti'
 
-export const filterInputExamplesVariants = [variant25, variant15, establishmentAutocomplete, multiselectExample, autoCompleteMultiExample ]
+export const filterInputExamplesVariants = [
+  variant25,
+  variant15,
+  establishmentAutocomplete,
+  multiselectExample,
+  autoCompleteMultiExample,
+]

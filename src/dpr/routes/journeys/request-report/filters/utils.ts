27c356aa7a1c70b510b940cbe2a706928e2fb518
rossdowthwaite <rossdowthwaite@gmail.com>
2026-06-10 09:49:44 +0100

    revert

--- src/dpr/routes/journeys/request-report/filters/utils.ts
+++ src/dpr/routes/journeys/request-report/filters/utils.ts
@@ -5,10 +5,11 @@ import { Request, Response, NextFunction } from 'express'
 import { buildFilterData, buildSortData } from '../../../../components/_async/async-filters-form/utils'
 import LocalsHelper from '../../../../utils/localsHelper'
 import { getRequestFilters } from '../../../../components/_filters/utils'
+import UserStoreItemBuilder from '../../../../utils/UserStoreItemBuilder'
 
 // Types
 import type ReportingService from '../../../../services/reportingService'
-import { ReportType } from '../../../../types/UserReports'
+import { ReportType, RequestFormData, RequestStatus } from '../../../../types/UserReports'
 import type { ExecutionData, ChildReportExecutionData } from '../../../../types/ExecutionData'
 import type { AsyncReportUtilsParams, RequestDataResult, RequestReportData } from '../../../../types/AsyncReportUtils'
 import type { SetQueryFromFiltersResult } from '../../../../components/_async/async-filters-form/types'
@@ -20,7 +21,6 @@ import { formBodyToQueryObject } from '../../../../utils/queryMappers'
 import { joinQueryStrings } from '../../../../utils/urlHelper'
 import { buildQuerySummary } from '../../../../components/_async/request-details/utils'
 import ReportQuery from '../../../../types/ReportQuery'
-import RequestedReportBuilder from '../../my-reports/requested-reports/builder'
 
 // ----------------------------------------------------------------------
 // POST
@@ -82,13 +82,51 @@ export const updateStore = async ({
   executionData: ExecutionData
   childExecutionData: Array<ChildReportExecutionData>
 }): Promise<void> => {
-  const { dprUser } = LocalsHelper.getValues(res)
+  const { type } = req.body
+  const { dprUser, definitionsPath, dpdPathFromQuery } = LocalsHelper.getValues(res)
 
-  const requestedReportData = new RequestedReportBuilder(req, res)
-    .withExecutionData(executionData)
-    .withChildExecutionData(childExecutionData)
-    .withQueryData(queryData)
-    .build()
+  const requestFormData: RequestFormData = req.body
+  const reportData = {
+    type: requestFormData.type as ReportType,
+    reportId: requestFormData.reportId,
+    reportName: requestFormData.reportName,
+    description: requestFormData.description,
+    id: requestFormData.id,
+    name: requestFormData.name,
+  }
+
+  let requestedReportData
+  switch (type) {
+    case ReportType.REPORT:
+      requestedReportData = new UserStoreItemBuilder(reportData, requestFormData)
+        .addExecutionData(executionData)
+        .addChildExecutionData(childExecutionData)
+        .addFilters(queryData?.filterData)
+        .addSortData(queryData?.sortData)
+        .addDefinitionsPath(definitionsPath, dpdPathFromQuery)
+        .addRequestUrls(req)
+        .addQuery(queryData)
+        .addStatus(RequestStatus.SUBMITTED)
+        .addTimestamp()
+        .build()
+      break
+    case ReportType.DASHBOARD: {
+      requestedReportData = new UserStoreItemBuilder(reportData, requestFormData)
+        .addExecutionData(executionData)
+        .addChildExecutionData(childExecutionData)
+        .addFilters(queryData?.filterData)
+        .addDefinitionsPath(definitionsPath, dpdPathFromQuery)
+        .addRequestUrls(req)
+        .addQuery(queryData)
+        .addStatus(RequestStatus.SUBMITTED)
+        .addTimestamp()
+        .addMetrics(JSON.parse(req.body.sections))
+        .build()
+      break
+    }
+    default:
+      break
+  }
 
   if (!requestedReportData) {
     return
@@ -161,7 +199,6 @@ const requestProduct = async ({
   let sortData: Record<string, string> | undefined
 
   const { definition, fields } = await getDefinitionByType(type, services, token, reportId, id)
-
   const query = new ReportQuery({
     fields,
     queryParams: formBodyToQueryObject(req.body),

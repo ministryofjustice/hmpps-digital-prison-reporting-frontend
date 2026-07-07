dd071a579f8602a190158e11c431a2d59ae9d85d
rossdowthwaite <rossdowthwaite@gmail.com>
2026-07-07 10:23:02 +0100

    Update sanitize config to be more strict

--- src/dpr/setUpNunjucksFilters.ts
+++ src/dpr/setUpNunjucksFilters.ts
@@ -47,10 +47,39 @@ const spacesToDash = (text: string) => {
 
 const safe = (text: string) => {
   const sanitized = sanitizeHtml(text, {
-    allowedTags: ['p', 'strong', 'em', 'ul', 'ol', 'li', 'a', 'br', 'div', 'h1', 'h2', 'h3', 'h4', 'br'],
+    allowedTags: ['p', 'strong', 'em', 'ul', 'ol', 'li', 'a', 'br', 'div', 'h1', 'h2', 'h3', 'h4'],
+
     allowedAttributes: {
       '*': ['class'],
       a: ['href', 'target', 'rel'],
+    },
+
+    allowedSchemes: ['https', 'mailto'],
+
+    allowProtocolRelative: false,
+
+    transformTags: {
+      a: (tagName, attribs) => {
+        const newAttribs = { ...attribs }
+
+        if (newAttribs['target'] && !['_blank', '_self'].includes(newAttribs['target'])) {
+          delete newAttribs['target']
+        }
+
+        if (newAttribs['target'] === '_blank') {
+          const rel = new Set((newAttribs['rel'] ?? '').split(/\s+/).filter(Boolean))
+
+          rel.add('noopener')
+          rel.add('noreferrer')
+
+          newAttribs['rel'] = [...rel].join(' ')
+        }
+
+        return {
+          tagName,
+          attribs: newAttribs,
+        }
+      },
     },
   })
 

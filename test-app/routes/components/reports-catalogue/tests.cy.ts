4584f8a2ea62939a39044ae1af5ec52381ca3289
rossdowthwaite <rossdowthwaite@gmail.com>
2026-08-25 11:07:03 +0100

    Catalogue test suite setup

---
+++ test-app/routes/components/reports-catalogue/tests.cy.ts
@@ -0,0 +1,85 @@
+describe('Reports Catalogue', () => {
+  describe('Filters', () => {
+    describe('Search', () => {
+      it('should match the product name and show all variants', () => {
+        //
+      })
+
+      it('should match a single variant name and the variant and the product row', () => {
+        //
+      })
+
+      it('should match the product and the variant and show the correct rows', () => {
+        //
+      })
+    })
+
+    describe('Type', () => {
+      it('should show both dashboards and reports when all is ticked', () => {
+        //
+      })
+
+      it('should show just dashboards when "Dashboards" is ticked', () => {
+        //
+      })
+
+      it('should show just reports when "Reports" is ticked', () => {
+        //
+      })
+    })
+
+    describe('Show unauthorised', () => {
+      it('should no show the unauthorised products when unchecked', () => {
+        //
+      })
+
+      it('should show the unauthorised products when checked', () => {
+        //
+      })
+    })
+
+    describe('Hide missing', () => {
+      it('should show the missing rows when unchecked', () => {
+        //
+      })
+
+      it('should not show the missing rows when checked', () => {
+        //
+      })
+    })
+
+    describe('Hide live', () => {
+      it('should show the live variant rows when unchecked', () => {
+        //
+      })
+
+      it('should not show live variant rows when checked', () => {
+        //
+      })
+    })
+
+    describe('Show/hide combination', () => {})
+  })
+
+  describe('Listing', () => {
+    describe('Totals', () => {
+      it('should show overall totals or reports and products in the catalogue', () => {
+        //
+      })
+    })
+
+    describe('Navigation', () => {
+      it('should show previous and next navigation and should work correctly', () => {
+        //
+      })
+
+      it('should not show the previous link on the first item', () => {
+        //
+      })
+
+      it('should not show the next link on the last item', () => {
+        //
+      })
+    })
+  })
+})

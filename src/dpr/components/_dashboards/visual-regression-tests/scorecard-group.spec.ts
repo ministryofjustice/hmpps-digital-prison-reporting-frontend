ae54f1b89be5c5bf16a6782f11d5dc682952dc12
Sunil Khutan <sunil.khutan@justice.gov.uk>
2026-09-15 11:03:06 +0100

    fix vrt test

--- src/dpr/components/_dashboards/visual-regression-tests/scorecard-group.spec.ts
+++ src/dpr/components/_dashboards/visual-regression-tests/scorecard-group.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { requestCatalogueVariant } from './vrtHelpers'
 
 test('Scorecard group VRT tests', async ({ page }) => {
@@ -11,6 +11,7 @@ test('Scorecard group VRT tests', async ({ page }) => {
   await page.getByRole('button', { name: /Request dashboard/ }).click()
 
   await expect(page.getByRole('heading', { name: 'Scorecard Group - Complete data' })).toBeVisible()
+  await expect(page.getByText('Value for').first()).toBeVisible() // ensures scorecard is fully loaded before taking screenshot
   await expect(page).toHaveScreenshot({
     fullPage: true,
     animations: 'disabled',

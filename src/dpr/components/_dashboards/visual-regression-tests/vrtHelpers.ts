c3770484489a282984c510d494b2b96411b16d8c
James Reed <james.reed1@justice.gov.uk>
2026-09-22 14:16:38 +0100

    linting

--- src/dpr/components/_dashboards/visual-regression-tests/vrtHelpers.ts
+++ src/dpr/components/_dashboards/visual-regression-tests/vrtHelpers.ts
@@ -17,26 +17,24 @@ export const requestCatalogueVariant = async (page: Page, name: string | RegExp)
 
 export const takeScreenshotsOfAllCharts = async (page: Page) => {
   const charts = await page.getByRole('tabpanel').all()
-  await Promise.all(charts.map(async (chart, idx) => {
-    await expect(chart).toHaveScreenshot(
-      `chart-${idx}.png`,
-      {
+  await Promise.all(
+    charts.map(async (chart, idx) => {
+      await expect(chart).toHaveScreenshot(`chart-${idx}.png`, {
         animations: 'disabled',
         maxDiffPixelRatio: 0.015,
-      }
+      })
+    }),
   )
-  }))
 }
 
 export const takeScreenshotsOfAllScorecards = async (page: Page) => {
   const charts = await page.locator('.dpr-scorecard').all()
-  await Promise.all(charts.map(async (chart, idx) => {
-    await expect(chart).toHaveScreenshot(
-      `scorecard-${idx}.png`,
-      {
+  await Promise.all(
+    charts.map(async (chart, idx) => {
+      await expect(chart).toHaveScreenshot(`scorecard-${idx}.png`, {
         animations: 'disabled',
         maxDiffPixelRatio: 0.015,
-      }
+      })
+    }),
   )
-  }))
 }

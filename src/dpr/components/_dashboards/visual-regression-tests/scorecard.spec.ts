86ca35124def34ebccbebb725059837bcbfb6f97
James Reed <109068236+ReedSoftware@users.noreply.github.com>
2026-09-23 13:46:08 +0100

    delete all screenshots, update tests to screenshot individual charts (#1165)

--- src/dpr/components/_dashboards/visual-regression-tests/scorecard.spec.ts
+++ src/dpr/components/_dashboards/visual-regression-tests/scorecard.spec.ts
@@ -1,5 +1,9 @@
 import { test, expect } from '@playwright/test'
-import { requestCatalogueVariant } from './vrtHelpers'
+import {
+  requestCatalogueVariant,
+  takeScreenshotsOfAllCharts,
+  takeScreenshotsOfAllScorecards,
+} from './helpers/vrtHelpers.spec'
 
 test('Scorecard VRT tests', async ({ page }) => {
   await page.goto('/embedded/platform')
@@ -11,9 +15,6 @@ test('Scorecard VRT tests', async ({ page }) => {
   await page.getByRole('button', { name: /Request dashboard/ }).click()
 
   await expect(page.getByRole('heading', { name: 'Scorecard - Complete data' })).toBeVisible()
-  await expect(page).toHaveScreenshot({
-    fullPage: true,
-    animations: 'disabled',
-    maxDiffPixelRatio: 0.015,
-  })
+  await takeScreenshotsOfAllScorecards(page)
+  await takeScreenshotsOfAllCharts(page)
 })
